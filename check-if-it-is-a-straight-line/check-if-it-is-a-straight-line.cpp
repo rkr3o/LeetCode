83c23a1ac4685bdd493724fb1d5da25090799ef1
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-06-19 23:51:22 +0530

    Time: 12 ms (39.61%) | Memory: 10 MB (56.99%) - LeetSync

--- check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.cpp
+++ check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
     bool checkStraightLine(vector<vector<int>>& coordinates) {
-        int cz = coordinates.size();
-        if (cz == 1) return false;  // One point only, not a straight line
-        if (cz == 2) return true;  // Two points only, always a straight line
+        int n = coordinates.size();
         
-        int x0 = coordinates[0][0], y0 = coordinates[0][1];
-        int dx = coordinates[1][0] - x0, dy = coordinates[1][1] - y0;
+        // Calculate the slope between the first two points
+        int x0 = coordinates[0][0];
+        int y0 = coordinates[0][1];
+        int x1 = coordinates[1][0];
+        int y1 = coordinates[1][1];
+        int slopeX = x1 - x0;
+        int slopeY = y1 - y0;
         
-        for (int i = 1; i < cz; i++) {   // Checking two point formula for each point with the first point
-            int x = coordinates[i][0], y = coordinates[i][1];
-            if (dx * (y - y0) != dy * (x - x0))  // Two point formula of line, if (x2-x1)*(y1-y0) = (x1-x0)(y2-y1), then a straight line, otherwise not
-			    return false;
+        // Check the slope between each pair of points
+        for (int i = 2; i < n; i++) {
+            int x = coordinates[i][0];
+            int y = coordinates[i][1];
+            
+            // Calculate the slope between the current point and the first point
+            int currSlopeX = x - x0;
+            int currSlopeY = y - y0;
+            
+            // If the slopes are not equal, the points do not lie on a straight line
+            if (slopeX * currSlopeY != slopeY * currSlopeX)
+                return false;
         }
+        
         return true;
     }
 };
