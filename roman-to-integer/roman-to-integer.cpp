785b2f74434d9265dbd0445b7d207ed9271ca85b
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-06-20 18:38:50 +0530

    Time: 14 ms (58.16%) | Memory: 7.9 MB (46.61%) - LeetSync

---
+++ roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int romanToInt(string s) 
+    {
+        unordered_map<char,int>mp;
+        mp['I']=1;
+        mp['V']=5;
+        mp['X']=10;
+        mp['L']=50;
+        mp['C']=100;
+        mp['D']=500;
+        mp['M']=1000;
+
+        int x = mp[s.back()];
+        for(int i = s.size()-2 ; i>=0 ; --i)
+        {
+            if(mp[s[i]] < mp[s[i+1]])
+            {
+                x-=mp[s[i]];
+            }
+            else
+            {
+                x+=mp[s[i]];
+            }
+        }
+        return x ;
+    }
+};
