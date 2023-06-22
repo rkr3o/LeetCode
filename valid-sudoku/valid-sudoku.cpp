5a3279506e09010ca050a34a44cd94660170fdb0
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-06-22 22:00:38 +0530

    Time: 34 ms (19.32%) | Memory: 21.5 MB (9.57%) - LeetSync

--- valid-sudoku/valid-sudoku.cpp
+++ valid-sudoku/valid-sudoku.cpp
@@ -1,30 +1,52 @@
 class Solution {
 public:
-    bool isValidSudoku(vector<vector<char>>& board) {
+    bool check(vector<char>&array)
+    {
+        unordered_set<int>st;
+        for(const auto &x : array)
+        {
+            if(x!='.' and st.count(x)>0)return false;
+            st.insert(x);
+        }
+        return true;
+    }
+    bool isValidSudoku(vector<vector<char>>& board) 
+    {
+        int n = board.size();
+        int m = board[0].size();
+        //valid row 
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(!check(board[i]))return false;
+        }
 
-//         Three Cases so 3 condtions.
-        int rowcase[9][9] ={0};
-        int colcase[9][9] = {0};
-        int gridcase[9][9] = {0};
+        // valid column 
+        for(int i = 0 ; i < n ; i++)
+        {
+            vector<char>column;
+            for(int j = 0 ; j < n ;j++)
+            {
+                column.push_back(board[j][i]);
+            }
 
-        for(int i=0;i<board.size();i++){
-            for(int j=0;j<board[0].size();j++){
-//                 if it is number.
-                if(board[i][j] != '.'){
-                    int number = board[i][j] - '0';
-
-
-                    int k = i/3 * 3 + j/3 ;
-                    // k denotes grid number
-
-                    // check for the 3 case.
-     if(rowcase[i][number-1]++ || colcase[j][number-1]++ || gridcase[k][number-1]++)
-                        return false;
-
+            if(!check(column))return false;
+        }
+        // valid each box
+        for(int boxRow = 0 ; boxRow < 3 ; boxRow++)
+        {
+            for(int boxCol  = 0 ; boxCol  < 3 ; boxCol++)
+            {
+                vector<char>box;
+                for(int row = boxRow*3 ; row < (boxRow+1)*3 ; row++)
+                {
+                   for(int col = boxCol*3 ; col < (1+boxCol)*3 ; col++)
+                   {
+                          box.push_back(board[row][col]);
+                   }
                 }
+                if(!check(box))return false;
             }
         }
         return true;
-
     }
 };
