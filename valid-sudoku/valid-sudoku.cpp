e2721db4e90c8dd7aa05214155c4040e56c45048
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-06-22 20:55:09 +0530

    Time: 24 ms (65.77%) | Memory: 18.1 MB (63.02%) - LeetSync

--- valid-sudoku/valid-sudoku.cpp
+++ valid-sudoku/valid-sudoku.cpp
@@ -1,51 +1,30 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        // Validate Rows
-        for (int row = 0; row < 9; row++) {
-            if (!isValidSet(board[row])) {
-                return false;
-            }
-        }
 
-        // Validate Columns
-        for (int col = 0; col < 9; col++) {
-            vector<char> column;
-            for (int row = 0; row < 9; row++) {
-                column.push_back(board[row][col]);
-            }
-            if (!isValidSet(column)) {
-                return false;
-            }
-        }
+//         Three Cases so 3 condtions.
+        int rowcase[9][9] ={0};
+        int colcase[9][9] = {0};
+        int gridcase[9][9] = {0};
 
-        // Validate Sub-boxes
-        for (int boxRow = 0; boxRow < 3; boxRow++) {
-            for (int boxCol = 0; boxCol < 3; boxCol++) {
-                vector<char> subBox;
-                for (int row = boxRow * 3; row < (boxRow + 1) * 3; row++) {
-                    for (int col = boxCol * 3; col < (boxCol + 1) * 3; col++) {
-                        subBox.push_back(board[row][col]);
-                    }
-                }
-                if (!isValidSet(subBox)) {
-                    return false;
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+//                 if it is number.
+                if(board[i][j] != '.'){
+                    int number = board[i][j] - '0';
+
+
+                    int k = i/3 * 3 + j/3 ;
+                    // k denotes grid number
+
+                    // check for the 3 case.
+     if(rowcase[i][number-1]++ || colcase[j][number-1]++ || gridcase[k][number-1]++)
+                        return false;
+
                 }
             }
         }
-
         return true;
-    }
 
-private:
-    bool isValidSet(const vector<char>& digits) {
-        unordered_set<char> digitSet;
-        for (char digit : digits) {
-            if (digit != '.' && digitSet.count(digit) > 0) {
-                return false;
-            }
-            digitSet.insert(digit);
-        }
-        return true;
     }
 };
