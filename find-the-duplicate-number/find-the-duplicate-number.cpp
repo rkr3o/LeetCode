453550060ad75d823c263dfe7e80250a43d370cd
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-06-23 18:38:24 +0530

    Time: 118 ms (75.51%) | Memory: 61.3 MB (45.66%) - LeetSync

--- find-the-duplicate-number/find-the-duplicate-number.cpp
+++ find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) 
-    {
-        int slow = nums[0];  // Move one step at a time
-        int fast = nums[0];  // Move two steps at a time
-
-        // Move the slow and fast pointers until they meet
-        while (true) 
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        
+        //check for cycle
+        while(true)
         {
-            slow = nums[slow];              // Move one step
-            fast = nums[nums[fast]];       // Move two steps
+            slow = nums[slow];//single stps
+            fast = nums[nums[fast]];//two steps
 
-            // Cycle detected
-            if (slow == fast) {
-                break;
-            }
+            if(slow==fast)break;
         }
-
-        // Move the slow pointer to the beginning
         slow = nums[0];
-
-        // Move both pointers one step at a time until they meet at the entrance of the cycle
-        while (slow != fast) {
+        while(slow!=fast)
+        {
             slow = nums[slow];
-            fast = nums[fast];
+            fast =  nums[fast];
         }
 
-        return slow;  // The duplicate number
-}
-
+        return slow;
+    }
 };
