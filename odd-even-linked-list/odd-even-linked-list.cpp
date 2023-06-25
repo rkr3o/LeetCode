292b8781eaef8a6e177b59d898d5e40db3a4c396
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-06-25 12:41:16 +0530

    Time: 14 ms (52.88%) | Memory: 10.5 MB (49.45%) - LeetSync

--- odd-even-linked-list/odd-even-linked-list.cpp
+++ odd-even-linked-list/odd-even-linked-list.cpp
@@ -1,38 +1,22 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if (head == nullptr) return nullptr;
-        if(head->next==NULL)return head;
-        vector<int> nodes;
-        ListNode* current = head;
-        while (current != nullptr) {
-            nodes.push_back(current->val);
-            current = current->next;
+        if (head == nullptr) {
+            return nullptr;
+        }
+        ListNode* oddHead = head;
+        ListNode* evenHead = head->next;
+        ListNode* odd = oddHead;
+        ListNode* even = evenHead;
+
+        while (even != nullptr && even->next != nullptr) {
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
         }
 
-        vector<int> odd, even;
-        for (int i = 0; i < nodes.size(); i++) {
-            if ((i+1) % 2 == 0) {
-                even.push_back(nodes[i]);
-            } else {
-                odd.push_back(nodes[i]);
-            }
-        }
-
-        ListNode* new_head = new ListNode(odd[0]);
-        ListNode* curr = new_head;
-        for (int i = 1; i < odd.size(); i++) {
-            curr->next = new ListNode(odd[i]);
-            curr = curr->next;
-        }
-
-        curr->next = new ListNode(even[0]);
-        curr = curr->next;
-        for (int i = 1; i < even.size(); i++) {
-            curr->next = new ListNode(even[i]);
-            curr = curr->next;
-        }
-
-        return new_head;
+        odd->next = evenHead;
+        return oddHead;
     }
 };
