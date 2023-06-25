a94d133f6f74ea7399aae862602d12f8bb90240f
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-06-25 22:54:40 +0530

    Time: 52 ms (28.80%) | Memory: 89.2 MB (96.79%) - LeetSync

--- median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -3,25 +3,38 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
         int m = nums2.size();
-        
-        nums1.push_back(INT_MAX);
-        nums2.push_back(INT_MAX);
-        
-        vector<int> merged(n + m);
-        int i = 0, j = 0, k = 0;
-        
-        while (k < n + m) {
-            if (nums1[i] <= nums2[j]) {
-                merged[k++] = nums1[i++];
+
+        // Ensure nums1 is the smaller array
+        if (n > m) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int left = 0;
+        int right = n;
+        int partitionX, partitionY;
+        int maxLeftX, maxLeftY, minRightX, minRightY;
+
+        while (left <= right) {
+            partitionX = (left + right) / 2;
+            partitionY = (n + m + 1) / 2 - partitionX;
+
+            maxLeftX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
+            minRightX = (partitionX == n) ? INT_MAX : nums1[partitionX];
+
+            maxLeftY = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
+            minRightY = (partitionY == m) ? INT_MAX : nums2[partitionY];
+
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                return (n + m) % 2 == 0 ? (max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2.0
+                                        : max(maxLeftX, maxLeftY);
+            } else if (maxLeftX > minRightY) {
+                right = partitionX - 1;
             } else {
-                merged[k++] = nums2[j++];
+                left = partitionX + 1;
             }
         }
-        
-        if ((n + m) % 2 == 1) {
-            return merged[(n + m) / 2];
-        } else {
-            return (double)(merged[(n + m) / 2 - 1] + merged[(n + m) / 2]) / 2;
-        }
+
+        // This should not happen if the input is valid
+        return -1;
     }
 };
