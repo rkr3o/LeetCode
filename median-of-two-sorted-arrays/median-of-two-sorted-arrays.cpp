692c7125a8da59c48f0ba60426dbb57d1f702b31
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-06-26 11:50:59 +0530

    Time: 36 ms (83.59%) | Memory: 89.3 MB (79.39%) - LeetSync

--- median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,40 +1,33 @@
-class Solution {
-public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
+class Solution
+{
+    public:
+        double findMedianSortedArrays(vector<int> &a, vector<int> &b)
+        {
 
-        // Ensure nums1 is the smaller array
-        if (n > m) {
-            return findMedianSortedArrays(nums2, nums1);
-        }
+            int n = a.size(), m = b.size();
+            int total = n + m;
+            int prev = 0, cur = 0;
 
-        int left = 0;
-        int right = n;
-        int partitionX, partitionY;
-        int maxLeftX, maxLeftY, minRightX, minRightY;
+            for (int k = 0, i = 0, j = 0; k <= total / 2; k++)
+            {
+                prev = cur;
+                if (i < n && (j >= m || a[i] <= b[j]))
+                {
+                    cur = a[i++];
+                }
+                else
+                {
+                    cur = b[j++];
+                }
+            }
 
-        while (left <= right) {
-            partitionX = (left + right) / 2;
-            partitionY = (n + m + 1) / 2 - partitionX;
-
-            maxLeftX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
-            minRightX = (partitionX == n) ? INT_MAX : nums1[partitionX];
-
-            maxLeftY = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
-            minRightY = (partitionY == m) ? INT_MAX : nums2[partitionY];
-
-            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
-                return (n + m) % 2 == 0 ? (max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2.0
-                                        : max(maxLeftX, maxLeftY);
-            } else if (maxLeftX > minRightY) {
-                right = partitionX - 1;
-            } else {
-                left = partitionX + 1;
+            if (total & 1)
+            {
+                return cur;
+            }
+            else
+            {
+                return (prev + cur) / 2.0;
             }
         }
-
-        // This should not happen if the input is valid
-        return -1;
-    }
 };
