d7e31eb7f912cc899aa39de02f03617219401564
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-06-27 20:57:10 +0530

    Time: 17 ms (47.83%) | Memory: 17.3 MB (99.29%) - LeetSync

--- asteroid-collision/asteroid-collision.cpp
+++ asteroid-collision/asteroid-collision.cpp
@@ -2,22 +2,17 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         vector<int> ans;
-
         for (int i = 0; i < asteroids.size(); i++) {
-            if (ans.empty() || asteroids[i] > 0) {
+            if (asteroids[i] > 0 || ans.empty() || ans.back() < 0) {
                 ans.push_back(asteroids[i]);
-            } else {
-                while (!ans.empty() && ans.back() > 0 && ans.back() < abs(asteroids[i])) {
-                    ans.pop_back();
+            } else if (ans.back() <= -asteroids[i]) {
+                if (ans.back() < -asteroids[i]) {
+                    i--;
                 }
-                if (ans.empty() || ans.back() < 0) {
-                    ans.push_back(asteroids[i]);
-                } else if (ans.back() == -asteroids[i]) {
-                    ans.pop_back();
-                }
+                ans.pop_back();
             }
         }
-
+        
         return ans;
     }
 };
