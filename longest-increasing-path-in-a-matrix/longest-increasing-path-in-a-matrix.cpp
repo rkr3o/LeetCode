ad14f773694bd3d0cfc73853e1173e6249cb6ac7
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-06-28 20:01:14 +0530

    Time: 74 ms (25.71%) | Memory: 17.7 MB (23.75%) - LeetSync

--- longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
+++ longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -1,36 +1,69 @@
+#include <vector>
+#include <queue>
+using namespace std;
+
 class Solution {
 public:
-    int dfs(int row , int col , int n , int m,vector<vector<int>>&matrix,vector<vector<int>>&dp)
-    {
-        int cnt = 0 ;
-        if(dp[row][col]!=-1)return dp[row][col];
-        int drow[]={-1,0,+1,0};
-        int dcol[]={0,+1,0,-1};
-        for(int i = 0 ; i < 4 ; i++)
-        {
-            int nrow=row+drow[i];
-            int ncol = col + dcol[i];
-            if(nrow>=0 and ncol >=0 and nrow<n and ncol < m and matrix[nrow][ncol]>matrix[row][col])
-            {
-                cnt = max(cnt,dfs(nrow,ncol,n,m , matrix,dp));
-            }
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        if (matrix.empty()) {
+            return 0;
         }
-        return dp[row][col] = cnt+1;
-    }
-    int longestIncreasingPath(vector<vector<int>>& matrix) 
-    {   
+        
         int n = matrix.size();
         int m = matrix[0].size();
-        int maxima = 0 ;
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        for(int i = 0 ; i < n ; i++)
-        {
-            for(int j = 0 ; j < m ; j++)
-            {
-                int path = dfs(i,j,n,m,matrix,dp);
-                maxima = max(maxima,path);
+        
+        vector<vector<int>> indegree(n, vector<int>(m, 0));
+        vector<vector<int>> directions{{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        
+        // Compute the indegree of each cell
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                for (const auto& dir : directions) {
+                    int newRow = i + dir[0];
+                    int newCol = j + dir[1];
+                    if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && matrix[newRow][newCol] > matrix[i][j]) {
+                        indegree[newRow][newCol]++;
+                    }
+                }
             }
         }
-        return maxima;
+        
+        // Find the cells with indegree 0 and enqueue them
+        queue<pair<int, int>> q;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (indegree[i][j] == 0) {
+                    q.push({i, j});
+                }
+            }
+        }
+        
+        // Perform BFS
+        int longestPath = 0;
+        while (!q.empty()) {
+            int size = q.size();
+            longestPath++;
+            
+            for (int i = 0; i < size; i++) {
+                int row = q.front().first;
+                int col = q.front().second;
+                q.pop();
+                
+                for (const auto& dir : directions) {
+                    int newRow = row + dir[0];
+                    int newCol = col + dir[1];
+                    
+                    if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && matrix[newRow][newCol] > matrix[row][col]) {
+                        indegree[newRow][newCol]--;
+                        
+                        if (indegree[newRow][newCol] == 0) {
+                            q.push({newRow, newCol});
+                        }
+                    }
+                }
+            }
+        }
+        
+        return longestPath;
     }
 };
