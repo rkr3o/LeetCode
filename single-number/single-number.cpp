142d15fb490a79748b8c67bb597753e9e1c9e3b6
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-06-29 14:25:36 +0530

    Time: 26 ms (36.69%) | Memory: 16.9 MB (67.85%) - LeetSync

--- single-number/single-number.cpp
+++ single-number/single-number.cpp
@@ -3,10 +3,7 @@ public:
     int singleNumber(vector<int>& nums) 
     {
        int xi = 0;
-       for(int i = 0 ; i < nums.size() ; i++)
-       {
-           xi = xi^nums[i];
-       } 
+       for(auto x : nums)xi = xi^x;
        return xi;
     }
 };
