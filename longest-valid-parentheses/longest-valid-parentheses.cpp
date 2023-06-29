748a9ea72e8a14bf6d2b7948183070a53b4fa167
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-06-29 14:20:40 +0530

    Time: 3 ms (86.36%) | Memory: 6.9 MB (93.50%) - LeetSync

--- longest-valid-parentheses/longest-valid-parentheses.cpp
+++ longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -1,24 +1,36 @@
-class Solution {
-public:
-    int longestValidParentheses(string s) {
-        stack<int>st;
-        st.push(-1);
-        int maxima =  0 ;
-        //use index 
-        for(int i = 0 ; i < s.size() ; i++)
+class Solution
+{
+    public:
+        int longestValidParentheses(string s)
         {
-            if(s[i]=='(')//valid start
-            {
-                st.push(i);
-            }
-            else
-            {
-                st.pop();
-                if(st.empty())st.push(i);
-                maxima = max(maxima,i-st.top());
-            }
-        }
 
-        return maxima;
-    }
+            int open = 0, close = 0;
+            int maxima = 0;
+           	//use index 
+            for (int i = 0; i < s.size(); i++)
+            {
+                if (s[i] == '(')
+                    open++;
+                else
+                    close++;
+                if (open == close)
+                    maxima = max(maxima, 2 *open);
+                if (close > open)
+                    open = close = 0;
+            }
+            open = close = 0;
+            for (int i = s.size() - 1; i >= 0; i--)
+            {
+                if (s[i] == '(')
+                    open++;
+                else
+                    close++;
+                if (open == close)
+                    maxima = max(maxima, 2 *open);
+                if (open > close)
+                    open = close = 0;
+            }
+
+            return maxima;
+        }
 };
