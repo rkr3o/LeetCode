f55d0b22e5efed8ff4e2aebf9697c473e7ab82f3
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-07-01 18:54:23 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (72.53%) - LeetSync

---
+++ decode-ways/decode-ways.cpp
@@ -0,0 +1,25 @@
+ class Solution
+ {
+     public:
+         int solve(const string &s, int idx , vector<int>&dp)
+         {
+             if (idx == s.size())return 1;	 
+             if (s[idx] == '0')return 0;	
+             if(dp[idx]!=-1)return dp[idx];
+             int cnt = 0;	 
+             cnt += solve(s, idx + 1,dp);	 
+             if (idx + 1 < s.size())
+             {
+                 int twoDigits = (s[idx] - '0') *10 + (s[idx + 1] - '0');
+                 if (twoDigits >= 10 && twoDigits <= 26)
+                 cnt += solve(s, idx + 2,dp); 
+             }
+             return dp[idx] = cnt;
+         }
+     int numDecodings(string s)
+     {  
+         int n = s.size();
+         vector<int>dp(n,-1);
+         return solve(s, 0 ,dp);
+     }
+ };
