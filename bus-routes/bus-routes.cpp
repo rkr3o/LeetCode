056d5b37b18e1d08f76043b7a45deea5e1dbc78c
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-07-01 19:57:18 +0530

    Time: 275 ms (66.52%) | Memory: 69.4 MB (38.86%) - LeetSync

--- bus-routes/bus-routes.cpp
+++ bus-routes/bus-routes.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
-    int numBusesToDestination(vector<vector<int>>& routes, int S, int T) {
-        unordered_map<int, vector<int>> to_routes;
-        for (int i = 0; i < routes.size(); ++i)
-            for (int j : routes[i])
-                to_routes[j].push_back(i);
-        queue<pair<int, int>> bfs;
-        bfs.push({S, 0});
-        unordered_set<int> seen = {S};
-        while (!bfs.empty()) {
-            int stop = bfs.front().first, bus = bfs.front().second;
-            bfs.pop();
-            if (stop == T)
-                return bus;
-            for (int i : to_routes[stop]) {
-                for (int j : routes[i]) {
-                    if (seen.find(j) == seen.end()) {
-                        seen.insert(j);
-                        bfs.push({j, bus + 1});
-                    }
-                }
-                routes[i].clear();
-            }
+    int numBusesToDestination(vector<vector<int>>& routes, int src, int t) {
+         unordered_map<int, vector<int>> mp;
+
+    // Mapping each bus stop to the routes that pass through it
+    for (int i = 0; i < routes.size(); i++) {
+        for (auto j : routes[i]) {
+            mp[j].push_back(i);
         }
-        return -1;
+    }
+    queue<pair<int, int>> q;
+    q.push({src, 0});
+    unordered_set<int> st = {src};
+    while (!q.empty()) {
+        int node = q.front().first;
+        int stops = q.front().second;
+        q.pop();
+
+        if (node == t)
+            return stops;
+
+        // Iterating over the routes passing through the current bus stop
+        for (auto x : mp[node]) {
+            // Iterating over the bus stops in the current route
+            for (auto y : routes[x]) {
+                if (st.find(y) == st.end()) {
+                    st.insert(y);
+                    q.push({y, stops + 1});
+                }
+            }
+            routes[x].clear();  // Marking the route as visited by clearing it
+        }
+    }
+    return -1;  // Destination not reachable
     }
 };
