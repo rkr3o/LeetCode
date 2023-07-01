52107909f92b7e6a71ca1a86af787117d5d2cf15
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-07-01 22:43:54 +0530

    Time: 7 ms (18.26%) | Memory: 5.9 MB (93.86%) - LeetSync

--- reverse-integer/reverse-integer.cpp
+++ reverse-integer/reverse-integer.cpp
@@ -1,35 +1,11 @@
-#include <algorithm> // Include the algorithm library for std::reverse
-#include <stdexcept> // Include the std::out_of_range exception
-
 class Solution {
 public:
-    int reverse(int x) 
-    {
-        if(x==0)return 0 ;
-         string s = to_string((x));
-         if (s.size() > 32) {
-             return 0;
-         }
-         while (s.back() == '0') {
-             s.pop_back(); 
-         }
-         std::reverse(s.begin(), s.end());
-         int sign = s.back();
-         if (sign == '-') {
-              s.pop_back();
-         }
-         while (s.back() == '0') {
-             s.pop_back();
-         }
-         int res = 0;
-         try {
-             res = std::stoi(s);
-         } catch (const std::out_of_range&) {
-             return 0;
-         }
-         if (sign == '-') {
-             res = -res;
-         }
-         return res;   
+    int reverse(int x) {
+        long long res = 0;
+        while(x) {
+            res = res*10 + x%10;
+            x /= 10;
+        }
+        return (res<INT_MIN || res>INT_MAX) ? 0 : res;
     }
 };
