2aa89ab75a7052e9238507168be0bc4c8267528a
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-07-01 19:08:13 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (29.20%) - LeetSync

--- decode-ways/decode-ways.cpp
+++ decode-ways/decode-ways.cpp
@@ -1,28 +1,29 @@
- class Solution
- {
-     public:
-         int solve(const string &s, int idx , vector<int>&dp)
-         {
-             if (idx == s.size())return 1;	 
-             if (s[idx] == '0')return 0;	
-             if(dp[idx]!=-1)return dp[idx];
-             int cnt = 0;	 
-             if(s[idx]!='0')
-             {
-                 cnt += solve(s, idx + 1,dp);	
-             } 
-             if (idx + 1 < s.size())
-             {
-                 int twoDigits = (s[idx] - '0') *10 + (s[idx + 1] - '0');
-                 if (twoDigits >= 10 && twoDigits <= 26)
-                 cnt += solve(s, idx + 2,dp); 
-             }
-             return dp[idx] = cnt;
-         }
-     int numDecodings(string s)
-     {  
-         int n = s.size();
-         vector<int>dp(n,-1);
-         return solve(s, 0 ,dp);
-     }
- };
+class Solution
+{
+public:
+    int numDecodings(string s)
+    {
+        int n = s.size();
+        vector<int> dp(n + 1, 0);
+        dp[n] = 1;
+        dp[n - 1] = (s[n - 1] != '0') ? 1 : 0; // Handle the base case for the last character
+
+        for (int i = n - 2; i >= 0; i--)
+        {
+            // Check if the current character is not '0' and can be decoded individually
+            if (s[i] != '0')
+            {
+                dp[i] = dp[i + 1];
+            }
+
+            // Check if the current and next characters can be decoded as a two-digit number
+            int twoDigits = (s[i] - '0') * 10 + (s[i + 1] - '0');
+            if (twoDigits >= 10 && twoDigits <= 26)
+            {
+                dp[i] += dp[i + 2];
+            }
+        }
+
+        return dp[0];
+    }
+};
