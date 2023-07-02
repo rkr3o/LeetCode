871f84fc3fd076cf9fe80081ecba19027a453bd0
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-07-02 10:36:24 +0530

    Time: 49 ms (39.65%) | Memory: 17.6 MB (99.91%) - LeetSync

--- reorder-list/reorder-list.cpp
+++ reorder-list/reorder-list.cpp
@@ -1,65 +1,69 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *    int val;
+ *    ListNode * next;
+ *    ListNode() : val(0), next(nullptr) {}
+ *    ListNode(int x) : val(x), next(nullptr) {}
+ *    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *};
  */
-class Solution {
-public:
-    void reverse(ListNode* &head) {
-        if (head == nullptr || head->next == nullptr) {
+class Solution
+{
+    public:
+        void reverse(ListNode* &head)
+        {
+            if (head == nullptr || head->next == nullptr)
+            {
+                return;
+            }
+
+            ListNode *prev = nullptr;
+            ListNode *cur = head;
+            ListNode *next = nullptr;
+
+            while (cur != nullptr)
+            {
+                next = cur->next;
+                cur->next = prev;
+                prev = cur;
+                cur = next;
+            }
+
+            head = prev;
+        }
+
+    void reorderList(ListNode *head)
+    {
+        if (head == nullptr || head->next == nullptr)
+        {
             return;
         }
-        
-        ListNode* prev = nullptr;
-        ListNode* cur = head;
-        ListNode* next = nullptr;
-        
-        while (cur != nullptr) {
-            next = cur->next;
-            cur->next = prev;
-            prev = cur;
-            cur = next;
-        }
-        
-        head = prev;
-    }
-    
-    void reorderList(ListNode* head) {
-        if (head == nullptr || head->next == nullptr) {
-            return;
-        }
-        
-        ListNode* slow = head;
-        ListNode* fast = head;
-        
-        while (fast != nullptr && fast->next != nullptr) {
+       	//find mid element and reverseve those elements and simple add it 
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while (fast != NULL and fast->next != NULL)
+        {
             slow = slow->next;
             fast = fast->next->next;
         }
-        
-        ListNode* head1 = head;
-        ListNode* head2 = slow->next;
-        slow->next = nullptr;
-        
+        ListNode *head1 = head;
+        ListNode *head2 = slow->next;
+        slow->next = NULL;
         reverse(head2);
-        
-        ListNode* temp1 = head1;
-        ListNode* temp2 = head2;
-        
-        while (temp1 != nullptr && temp2 != nullptr) {
-            ListNode* next1 = temp1->next;
-            ListNode* next2 = temp2->next;
-            
-            temp1->next = temp2;
-            temp2->next = next1;
-            
-            temp1 = next1;
-            temp2 = next2;
+
+        ListNode *temp1 = NULL;
+        ListNode *temp2 = NULL;
+        while (head2 != NULL)
+        {
+            temp1 = head1->next;
+            temp2 = head2->next;
+            head1->next = head2;
+            head2->next = temp1;
+
+            head1 = temp1;
+            head2 = temp2;
         }
     }
 };
