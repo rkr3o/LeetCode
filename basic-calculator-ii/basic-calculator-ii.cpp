4601f966b33b93f26f0086891046fb8ea9e10d9e
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-07-04 11:48:53 +0530

    Time: 12 ms (94.77%) | Memory: 11.2 MB (72.74%) - LeetSync

--- basic-calculator-ii/basic-calculator-ii.cpp
+++ basic-calculator-ii/basic-calculator-ii.cpp
@@ -1,36 +1,52 @@
-#include <string>
-
-class Solution {
-public:
-    int calculate(string s) {
-        int total = 0;
-        int num = 0;
-        int prevNum = 0;
-        char sign = '+';
-
-        for (int i = 0; i < s.length(); i++) {
-            char c = s[i];
-
-            if (isdigit(c)) {
-                num = num * 10 + (c - '0');
-            }
-
-            if ((!isdigit(c) && c != ' ') || i == s.length() - 1) {
-                if (sign == '+' || sign == '-') {
-                    total += prevNum;
-                    prevNum = (sign == '+') ? num : -num;
-                } else if (sign == '*') {
-                    prevNum *= num;
-                } else if (sign == '/') {
-                    prevNum /= num;
+class Solution
+{
+    public:
+        int calculate(string s)
+        {
+            stack<int> st;
+            int num = 0;
+            char sign ='+';
+            for (int i = 0; i < s.size(); i++)
+            {
+                char c = s[i];
+                if (isdigit(c))
+                {
+                    num = num*10 + (c - '0');
                 }
 
-                num = 0;
-                sign = c;
+                if ((!isdigit(c) and c != ' ') || i == s.length()-1)
+                {
+                    if (sign == '+')
+                    {
+                        st.push(num);
+                    }
+                    else if (sign == '-')
+                    {
+                        st.push(-num);
+                    }
+                    else if (sign == '*')
+                    {
+                        int topEle = st.top();
+                        st.pop();
+                        st.push(topEle*num);
+                    }
+                    else if (sign == '/')
+                    {
+                        int topEle = st.top();
+                        st.pop();
+                        st.push(topEle / num);
+                    }
+                    num = 0;
+                    sign=c;
+                }
             }
-        }
+            int result = 0;
+            while (!st.empty())
+            {
+                result += st.top();
+                st.pop();
+            }
 
-        total += prevNum;
-        return total;
-    }
+            return result;
+        }
 };
