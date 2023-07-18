0df910e3abaf1e60d6e0d6a4c78350d963ef45fd
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-07-18 19:28:16 +0530

    Time: 3 ms (47.60%) | Memory: 5.9 MB (74.76%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
+   int dp[1000];
     int catalan(int n)
     {
         if(n<=1)return 1 ;
+        if(dp[n]!=-1)return dp[n];
         int ans = 0 ;
         for(int i = 0 ; i < n ; i++)
         {
             ans+=catalan(i)*catalan(n-i-1);
         }
-        return ans;
+        return dp[n] = ans;
     }
     int numTrees(int n) {
-        return catalan(n);
+      memset(dp,-1,sizeof(dp));
+           return catalan(n);
     }
 };
