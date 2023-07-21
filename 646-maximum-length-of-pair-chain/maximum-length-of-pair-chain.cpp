7099da3fe064691b09dd636695456bdf4afa4227
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-07-21 13:56:21 +0530

    Time: 68 ms (89.38%) | Memory: 22.5 MB (74.27%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
@@ -1,25 +1,26 @@
 #include <vector>
 #include <algorithm>
+#include <limits>
 
 class Solution {
 public:
     int findLongestChain(std::vector<std::vector<int>>& pairs) {
         int n = pairs.size();
-        if (n == 1) return 1;
-
         std::sort(pairs.begin(), pairs.end(), [](const std::vector<int>& a, const std::vector<int>& b) {
-            return a[0] < b[0];
+            return a[1] < b[1];
         });
 
-        std::vector<int> dp(n, 1);
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (pairs[i][0] > pairs[j][1]) {
-                    dp[i] = std::max(dp[i], dp[j] + 1);
-                }
+        int ans = 0;
+        int cur = std::numeric_limits<int>::min();
+        for (const auto& pair : pairs) {
+            int head = pair[0];
+            int tail = pair[1];
+            if (head > cur) {
+                cur = tail;
+                ans++;
             }
         }
 
-        return *std::max_element(dp.begin(), dp.end());
+        return ans;
     }
 };
