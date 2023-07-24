5b37fd6482747a4e15c7944efc66bd406243d5ba
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-07-24 16:09:41 +0530

    Time: 115 ms (27.48%) | Memory: 37.3 MB (47.68%) - LeetSync

--- 852-friends-of-appropriate-ages/friends-of-appropriate-ages.cpp
+++ 852-friends-of-appropriate-ages/friends-of-appropriate-ages.cpp
@@ -1,30 +1,16 @@
 class Solution {
 public:
-    int numFriendRequests(vector<int>& ages) {
-        vector<int> ageCount(121, 0); // Age count array, maximum age is 120
-
-        // Count the number of people in each age group
-        for (int age : ages) {
-            ageCount[age]++;
+     
+      int numFriendRequests(vector<int>& ages) {
+        sort(ages.begin(), ages.end());
+        int res = 0;
+        for (int i = 0; i < ages.size(); ++i) {
+            int low_bound = ages[i]/2 + 8;
+            auto low_it = lower_bound(ages.begin(), ages.end(), low_bound);
+            auto high_it = upper_bound(ages.begin(), ages.end(), ages[i]);
+            int request = distance(low_it, high_it) - 1;
+            res += max(request, 0);
         }
-
-        int cnt = 0;
-
-        // Calculate the number of friend requests for each age group
-        for (int ageA = 1; ageA <= 120; ageA++) {
-            int countA = ageCount[ageA];
-            for (int ageB = 0.5 * ageA + 8; ageB <= ageA; ageB++) {
-                int countB = ageCount[ageB];
-
-                // Don't count requests from a person to themselves
-                if (ageA == ageB) {
-                    cnt += countA * (countA - 1);
-                } else {
-                    cnt += countA * countB;
-                }
-            }
-        }
-
-        return cnt;
+        return res;
     }
 };
