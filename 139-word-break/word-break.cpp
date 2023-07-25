e9d0c5e671cfb51c15daf8818ee31e59130d4772
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-07-25 16:45:27 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (56.14%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,30 +1,88 @@
+struct Node {
+    Node* links[26];
+    bool end = false;
+
+    Node* get(char ch) {
+        return links[ch - 'a'];
+    }
+    
+    bool containKey(char ch) {
+        return links[ch - 'a'] != NULL;
+    }
+    
+    void put(char ch, Node* node) {
+        links[ch - 'a'] = node;
+    }
+    
+    void setEnd() {
+        end = true;
+    }
+    
+    bool isEnd() {
+        return end;
+    }
+};
+
+class Trie {
+private:
+   
+
+public: 
+    Node* root;
+    Trie() {
+        root = new Node();
+    }
+
+    void insert(const string& word) {
+        Node* node = root;
+        for (char ch : word) {
+            if (!node->containKey(ch)) {
+                node->put(ch, new Node());
+            }
+            node = node->get(ch);
+        }
+        node->setEnd();
+    }
+
+    bool search(const string& word) {
+        Node* node = root;
+        for (char ch : word) {
+            if (!node->containKey(ch)) {
+                return false;
+            }
+            node = node->get(ch);
+        }
+        return node->isEnd();
+    }
+};
+
 class Solution {
 public:
-    int dp[300];
-    bool solve(int ind , string &s , unordered_set<string>&st)
-    {
-        if(ind==s.size())
-        {
-            return true;
+    bool wordBreak(string s, vector<string>& wordDict) {
+        Trie trie;
+        for (string& word : wordDict) {
+            trie.insert(word);
         }
-        if(dp[ind]!=-1)return dp[ind];
-        string req ;
-        for(int i = ind ; i < s.size() ; i++)
-        {
-            req+=s[i];
-            if(st.find(req)!=st.end())
-            {
-                if(solve(i+1,s,st)==true)
-                {
-                    return dp[ind]=true;
+
+        int n = s.length();
+        vector<bool> dp(n + 1, false);
+        dp[n] = true;
+
+        for (int i = n - 1; i >= 0; i--) {
+            Node* cur = trie.root; // Use Node* instead of TrieNode*
+            for (int j = i + 1; j <= n; j++) {
+                char c = s[j - 1]; // Use A[j-1] instead of s[j-1]
+                if (!cur->containKey(c)) { // Use containKey instead of search
+                    break; // A[i:j] does not exist in our trie
+                }
+                cur = cur->get(c);
+                if (cur->isEnd() && dp[j]) {
+                    dp[i] = true;
+                    break;
                 }
             }
         }
-     return dp[ind] =  false;
-    }
-    bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string>st(wordDict.begin(),wordDict.end());
-        memset(dp,-1,sizeof(dp));
-        return solve(0,s,st);
+
+        return dp[0];
     }
 };
