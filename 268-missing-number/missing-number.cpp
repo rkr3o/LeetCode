ae76a83985efbad255642c2d46cede93095298d3
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-07-27 01:14:33 +0530

    Time: 17 ms (87.25%) | Memory: 17.9 MB (84.82%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+        int xorr = 0, xor2 = 0;
+        for (int i = 0; i < nums.size(); i++) 
+        {
+            xorr = xorr ^ i;
+            xor2= xor2^ nums[i];
+        }
+	   xor2 = xor2 ^ nums.size();
+       return xorr^xor2;
+    }
+};
