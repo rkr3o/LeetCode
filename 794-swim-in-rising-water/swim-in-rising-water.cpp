033c77201bbe2a3e1fec5967f44e2d3c5ddd42e3
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-07-27 18:16:33 +0530

    Time: 7 ms (99.87%) | Memory: 8.9 MB (75.47%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -8,31 +8,35 @@ public:
         };
         priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, decltype(compare)> pq(compare);
         pq.push({grid[0][0], {0, 0}});
-        vector<vector<int>> seen(n, vector<int>(n, 0));
-        seen[0][0] = 1;
-        static int dir[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        vector<vector<int>> vis(n, vector<int>(n, 0));
+        vis[0][0] = 1;
+        int drow[]={-1,0,+1,0};
+        int dcol[]={0,+1,0,-1};
 
-        while (true) {
+        while (!pq.empty()) 
+        {
             auto p = pq.top();
             pq.pop();
-            int t = p.first;
-            int x = p.second.first;
-            int y = p.second.second;
+            int element = p.first;
+            int row = p.second.first;
+            int col = p.second.second;
 
-            res = max(res, t);
+            res = max(res, element);
 
-            if (x == n - 1 && y == n - 1)
+            if (row == n - 1 && col == n - 1)
                 return res;
-
-            for (auto& d : dir) {
-                int i = x + d[0];
-                int j = y + d[1];
-
-                if (i >= 0 && i < n && j >= 0 && j < n && !seen[i][j]) {
-                    seen[i][j] = 1;
-                    pq.push({grid[i][j], {i, j}});
+            for(int i = 0 ; i < 4 ; i++)
+            {
+                int nrow = row+drow[i];
+                int ncol = col + dcol[i];
+                if(nrow>=0 and ncol >=0 and nrow< n and ncol < n and !vis[nrow][ncol])
+                {
+                    vis[nrow][ncol]=1;
+                    pq.push({grid[nrow][ncol] , {nrow,ncol}});
                 }
             }
+             
         }
+        return -1;
     }
 };
