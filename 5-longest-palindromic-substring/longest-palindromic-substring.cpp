3531c1c0e883425cda127323f24804f09c34c2c0
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-07-29 19:20:18 +0530

    Time: 7 ms (97.50%) | Memory: 7 MB (74.48%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-     bool checkPalindrome(string &s)
-    {
-        int st = 0 ;
-        int end = s.size()-1;
-        while(st<=end)
-        {
-            if(s[st++]!=s[end--])
-            {
-                return false;
-            }
+    int expandFromMiddle(const string& s, int st, int end) {
+        int n = s.size();
+        while (st >= 0 && end < n && s[st] == s[end]) {
+            st--;
+            end++;
         }
-        return true;
+        return end - st - 1;
     }
     string longestPalindrome(string s) {
-       string maxima="";
         int n = s.size();
-        for(int i = 0 ; i < n ; i++)
-        {
-            string cur;
-            for(int j = i ; j < n ;j++)
-            {
-                cur+=s[j];
-                if(checkPalindrome(cur)==true and cur.size() > maxima.size())
-                {
-                    maxima = cur;
-                }
+        if (n == 0) return "";
+
+        int maxima = 1;
+        int start = 0; // Starting index of the longest palindromic substring
+
+        for (int i = 0; i < n; i++) {
+            int even = expandFromMiddle(s, i, i);
+            int odd = expandFromMiddle(s, i, i + 1);
+
+            int cur = max(even, odd);
+
+            if (cur > maxima) {
+                maxima = cur;
+                start = i - (cur - 1) / 2; // Calculate the starting index of the new longest palindrome
             }
         }
-        return maxima;
+
+        // Extract the longest palindromic substring
+        return s.substr(start, maxima);
     }
 };
