a9df0f77e04d9ad8cf0b19dc2557e1dd21ef9595
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-08-06 10:56:10 +0530

    Time: 3 ms (95.02%) | Memory: 13.2 MB (39.81%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,46 +1,14 @@
 class Solution {
 public:
-    const int mod = 1e9+7;
-    int solve(int i , int buy , vector<int>&prices , vector<vector<int>>&dp)
-    {
-        if(i>=prices.size())return 0;
-        if(dp[i][buy]!=-1)return dp[i][buy];
-        int ans = INT_MIN ;
-        //buy part
-
-        if(buy==1)
-        {
-               ans = max(prices[i]+solve(i+1,0,prices,dp) , solve(i+1,1,prices,dp));
-        } 
-        //sell part
-        if(buy==0)
-        {
-             ans=max(-prices[i]+solve(i+1,1,prices,dp),solve(i+1,0,prices,dp));
-        }
-        return dp[i][buy] = ans;
-    }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>>dp(n+1,vector<int>(2,0));
-        
-        for(int i = n-1 ; i >=0 ; i--)
+        if(n<=1)return 0;
+        int maxima = 0 ;
+        //preety simple if i-1th day is small means we can but it and sell it to ith day and we need total sum
+        for(int i =1 ; i < n ; i++)
         {
-            for(int buy = 0 ; buy<=1 ; buy++)
-            {
-                int profit = 0 ;
-               if(buy)
-                {
-                    profit = max(-prices[i]+dp[i+1][0], dp[i+1][1]);
-                }
-                else 
-                {
-                    profit = max(prices[i]+dp[i+1][1], dp[i+1][0]);
-                }
-
-                dp[i][buy]=profit;
-            }
-        } 
-        return dp[0][1];
-
+            maxima += max(0,prices[i]-prices[i-1]);
+        }
+        return maxima;
     }
 };
