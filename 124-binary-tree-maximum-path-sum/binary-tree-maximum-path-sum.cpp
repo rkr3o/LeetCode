949ced27ddcf44543004131a3968539c5751c216
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-08-08 22:43:03 +0530

    Time: 26 ms (64.13%) | Memory: 27.7 MB (47.16%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -1,26 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-       int getMaxPathSum(TreeNode* node, int& maxSum) {
-        if (node == nullptr) return 0;
-
-        int leftSum = getMaxPathSum(node->left, maxSum);
-        int rightSum = getMaxPathSum(node->right, maxSum);
-
-        // Calculate the sum of the current path
-        int currentSum = node->val + max(0, leftSum) + max(0, rightSum);
-
-        // Update the maximum sum if the current path is greater
-        maxSum = max(maxSum, currentSum);
-
-        // Return the maximum sum of the current node's subtree
-        return node->val + max(0, max(leftSum, rightSum));
+    int findMaxPathSum(TreeNode* root , int &maxima)
+    {
+        if (root == NULL) return 0;
+        int leftMaxPath = max(0, findMaxPathSum(root -> left, maxima));
+        int rightMaxPath = max(0, findMaxPathSum(root -> right, maxima));
+        int val = root -> val;
+        maxima = max(maxima, (leftMaxPath + rightMaxPath + val));
+        return root->val+max(leftMaxPath, rightMaxPath);
     }
     int maxPathSum(TreeNode* root) {
-        if (root == nullptr) return 0;
-
-        int maxSum = INT_MIN;
-        getMaxPathSum(root, maxSum);
-
-        return maxSum;
+        if(root==NULL)return 0 ; 
+        int maxi = INT_MIN;
+        findMaxPathSum(root, maxi);
+        return maxi;
     }
 };
