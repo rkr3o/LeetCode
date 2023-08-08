443f4b86b4c7ca059d1e38041173fe7d0bc0d23a
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-08-08 12:07:27 +0530

    Time: 0 ms (100.00%) | Memory: 6.1 MB (59.70%) - LeetSync

--- 420-strong-password-checker/strong-password-checker.cpp
+++ 420-strong-password-checker/strong-password-checker.cpp
@@ -9,31 +9,37 @@ class Solution {
         }
         return lowercase + uppercase + digit;
     }
-public:
-    int strongPasswordChecker(string s) {
-        int requiredChar = getRequiredChar(s);
-        if (s.length() < 6) return max(requiredChar, 6 - (int)(s.length()));
-        int replace = 0;
-        int oned = 0;
-        int twod = 0;
-
-        for (int i = 0; i < s.length();) {
+    
+    void countRepeatingGroups(string s, int &replace, int &oned, int &twod) {
+        for (int i = 0; i < s.length(); ) {
             int len = 1;
             while (i + len < s.length() && s[i + len] == s[i + len - 1]) len++;
             if (len >= 3) {
                 replace += len / 3;
-                if (len % 3 == 0) oned += 1;
-                if (len % 3 == 1) twod += 2;
+                if (len % 3 == 0) oned++;
+                else if (len % 3 == 1) twod += 2;
             }
             i += len;
         }
+    }
+    
+public:
+    int strongPasswordChecker(string s) {
+        int requiredChar = getRequiredChar(s);
+        int n = s.length();
 
-        if (s.length() <= 20) return max(requiredChar, replace);
+        if (n < 6) return max(requiredChar, 6 - n);
+        
+        int replace = 0, oned = 0, twod = 0;
+        countRepeatingGroups(s, replace, oned, twod);
 
-        int deleteCount = s.length() - 20;
+        if (n <= 20) return max(requiredChar, replace);
+
+        int deleteCount = n - 20;
         replace -= min(deleteCount, oned);
         replace -= min(max(deleteCount - oned, 0), twod) / 2;
         replace -= max(deleteCount - oned - twod, 0) / 3;
+
         return deleteCount + max(requiredChar, replace);
     }
 };
