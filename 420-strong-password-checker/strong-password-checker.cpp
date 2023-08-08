2e418a579e15bf93bb804bbdf05f51012abfcb49
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-08-08 12:09:37 +0530

    Time: 2 ms (38.81%) | Memory: 6.1 MB (59.70%) - LeetSync

--- 420-strong-password-checker/strong-password-checker.cpp
+++ 420-strong-password-checker/strong-password-checker.cpp
@@ -1,5 +1,4 @@
-class Solution {
-    
+class Solution { 
     int getRequiredChar(string s) {
         int lowercase = 1, uppercase = 1, digit = 1;
         for (char c : s) {
@@ -9,32 +8,31 @@ class Solution {
         }
         return lowercase + uppercase + digit;
     }
-    
     void countRepeatingGroups(string s, int &replace, int &oned, int &twod) {
-        for (int i = 0; i < s.length(); ) {
+        int i = 0, n = s.length();
+
+        while (i < n) {
             int len = 1;
-            while (i + len < s.length() && s[i + len] == s[i + len - 1]) len++;
+            while (i + len < n && s[i + len] == s[i + len - 1]) len++;
+
             if (len >= 3) {
                 replace += len / 3;
                 if (len % 3 == 0) oned++;
                 else if (len % 3 == 1) twod += 2;
             }
+
             i += len;
         }
     }
-    
 public:
     int strongPasswordChecker(string s) {
         int requiredChar = getRequiredChar(s);
         int n = s.length();
-
         if (n < 6) return max(requiredChar, 6 - n);
-        
         int replace = 0, oned = 0, twod = 0;
         countRepeatingGroups(s, replace, oned, twod);
 
         if (n <= 20) return max(requiredChar, replace);
-
         int deleteCount = n - 20;
         replace -= min(deleteCount, oned);
         replace -= min(max(deleteCount - oned, 0), twod) / 2;
