40a3c148d4b70303a50c6c723806cfc3c5e25b63
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-08-08 12:32:30 +0530

    Time: 8 ms (69.67%) | Memory: 8.8 MB (32.04%) - LeetSync

---
+++ 725-split-linked-list-in-parts/split-linked-list-in-parts.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int getSize(ListNode* head) {
+        int cnt = 0;
+        while (head != NULL) {
+            cnt++;
+            head = head->next;
+        }
+        return cnt;
+    }
+
+    vector<ListNode*> splitListToParts(ListNode* head, int k) {
+        vector<ListNode*> ans(k, NULL);
+
+        if (head == NULL) return ans;
+
+        int n = getSize(head);
+
+        int partSize = n / k;
+        int extraParts = n % k;
+
+        ListNode* temp = head;
+
+        for (int i = 0; i < k; i++) {
+            ans[i] = temp;
+
+            for (int j = 1; j < partSize + (i < extraParts); j++) {
+                if (temp == NULL) {
+                 //   cout << "Error: Reached end of list prematurely in part " << i << endl;
+                    return ans; // Return to identify the issue
+                }
+                temp = temp->next;
+            }
+
+            if (temp != nullptr) {
+                ListNode* nextNode = temp->next;
+                temp->next = nullptr;
+                temp = nextNode;
+            }
+        }
+
+        return ans;
+    }
+};
