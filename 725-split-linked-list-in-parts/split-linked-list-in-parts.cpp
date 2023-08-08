49a96f551331a8a92e8ccf3224e0c011071339b3
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-08-08 12:36:50 +0530

    Time: 3 ms (97.49%) | Memory: 8.9 MB (9.49%) - LeetSync

--- 725-split-linked-list-in-parts/split-linked-list-in-parts.cpp
+++ 725-split-linked-list-in-parts/split-linked-list-in-parts.cpp
@@ -18,20 +18,17 @@ public:
 
         int partSize = n / k;
         int extraParts = n % k;
-
         ListNode* temp = head;
-
         for (int i = 0; i < k; i++) {
             ans[i] = temp;
-
             for (int j = 1; j < partSize + (i < extraParts); j++) {
                 if (temp == NULL) {
-                 //   cout << "Error: Reached end of list prematurely in part " << i << endl;
+                    //cout << "Error: Reached end of list prematurely in part " << i << endl;
                     return ans; // Return to identify the issue
                 }
                 temp = temp->next;
             }
-
+            //disconnect the part 
             if (temp != nullptr) {
                 ListNode* nextNode = temp->next;
                 temp->next = nullptr;
