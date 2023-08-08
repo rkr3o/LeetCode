9ac0330965584a9d4f9009de9112b203446784e2
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-08-08 20:42:54 +0530

    Time: 7 ms (94.35%) | Memory: 21.1 MB (14.71%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -13,22 +13,28 @@ class Solution {
 public:
     int getHeight(TreeNode* root)
     {
-        if(root==NULL)return 0;
-
-        int left = getHeight(root->left);
-        int right = getHeight(root->right);
-
-        return 1+max(left,right);
+        if(root==NULL)
+        {
+            return 0 ;
+        }
+        // balanced tree means  the diff btwn left and right <=1
+        
+        int left  = getHeight (root->left);
+        int right  = getHeight(root->right);
+        if(left==-1 || right ==-1)
+        {
+            return-1;
+        }
+        if(abs(left-right)>1)
+        {
+            return -1 ;
+        }
+        return 1+max(left ,right);
     }
     bool isBalanced(TreeNode* root) {
         //abs(leftHeight-rightRight) <=1
 
         if(root==NULL)return true;
-        int lh = getHeight(root->left);
-        int rh = getHeight(root->right);
-        bool left = isBalanced(root->left);
-        bool right = isBalanced(root->right);
-
-        return (abs(lh-rh)<=1) and left and right;
+         return getHeight(root)!=-1;
     }
 };
