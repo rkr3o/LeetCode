742626d499de2a79d6e2e37bf35d32eaedef3fb5
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-08-09 15:18:30 +0530

    Time: 5 ms (54.03%) | Memory: 13.8 MB (14.53%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,38 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int, map<int, multiset<int>>> mp;
-        queue<pair<int, pair<int, TreeNode*>>> q;
-        q.push({0, {0, root}});
-        
-        while (!q.empty()) {
+         vector<vector<int>>ans;
+        map<int,map<int,multiset<int>>>mp;
+        queue<pair<int,pair<int,TreeNode*>>>q;
+        q.push({0,{0,root}});
+        while(!q.empty())
+        {
             auto x = q.front();
-            int hd = x.first;
+            q.pop();
+            int hd= x.first;
             int lvl = x.second.first;
             TreeNode* node = x.second.second;
-            q.pop();
-            
             mp[hd][lvl].insert(node->val);
-            
-            if (node->left) {
-                q.push({hd - 1, {lvl + 1, node->left}});
+
+            if(node->left)
+            {
+                q.push({hd-1,{lvl+1,node->left}});
             }
-            if (node->right) {
-                q.push({hd + 1, {lvl + 1, node->right}});
+            if(node->right)
+            {
+                q.push({hd+1,{lvl+1,node->right}});
             }
         }
-        
-        vector<vector<int>> ans;
-        for (auto x : mp) {
-            vector<int> column;
-            for (auto y : x.second) {
-                for (auto z : y.second) {
-                    column.push_back(z);
-                }
+
+    for(auto x : mp)
+    {
+        vector<int>temp;
+        for(auto y : x.second)
+        {
+            for(auto z : y.second)
+            {
+                 temp.push_back(z);
             }
-            ans.push_back(column);
         }
-        
-        return ans;
+        ans.push_back(temp);
+    }
+
+    return ans;
     }
 };
