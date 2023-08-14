b67d582ec7bc58c46c7735e987a52d3524bd6b75
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-08-14 12:21:09 +0530

    Time: 15 ms (63.79%) | Memory: 24 MB (20.96%) - LeetSync

--- 669-trim-a-binary-search-tree/trim-a-binary-search-tree.cpp
+++ 669-trim-a-binary-search-tree/trim-a-binary-search-tree.cpp
@@ -1,31 +1,19 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     TreeNode* trimBST(TreeNode* root, int low, int high) {
         if (root == nullptr) return nullptr;
 
         if (root->val < low) {
-            // If the root's value is less than low, recursively trim right subtree
+            // If root's value is less than low, the trimmed tree will be in the right subtree
             return trimBST(root->right, low, high);
         } else if (root->val > high) {
-            // If the root's value is greater than high, recursively trim left subtree
+            // If root's value is greater than high, the trimmed tree will be in the left subtree
             return trimBST(root->left, low, high);
+        } else {
+            // Root's value is within the range, so recursively trim both left and right subtrees
+            root->left = trimBST(root->left, low, high);
+            root->right = trimBST(root->right, low, high);
+            return root;
         }
-
-        // Trim both left and right subtrees and update root's children
-        root->left = trimBST(root->left, low, high);
-        root->right = trimBST(root->right, low, high);
-
-        return root;
     }
 };
