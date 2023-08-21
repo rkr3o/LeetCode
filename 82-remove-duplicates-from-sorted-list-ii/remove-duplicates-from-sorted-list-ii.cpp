17f0df60ac3cbfa64e268bc628a1985d0994220b
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-08-21 12:36:14 +0530

    Time: 3 ms (95.93%) | Memory: 11.1 MB (42.21%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
@@ -1,50 +1,31 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        set<int>st;
-        vector<int>ans;
-        ListNode* temp = head;
-        while(temp!=NULL)
-        {
-            ans.push_back(temp->val);
-            temp=temp->next;
-        }
-        for(int i =1; i < ans.size(); i++)
-        {
-            if(ans[i]==ans[i-1])
-            {
-                st.insert(ans[i]);
+        if (!head || !head->next)
+            return head;
+
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+
+        ListNode* prev = dummy;
+        ListNode* current = head;
+
+        while (current) {
+            bool isDuplicate = false;
+            while (current->next && current->val == current->next->val) {
+                current = current->next;
+                isDuplicate = true;
             }
-        }
-        ListNode* newNode = NULL;
-        ListNode* tail = NULL;
-        while(head!=NULL)
-        {
-            if(st.find(head->val)==st.end())
-            {
-                if(newNode==NULL)
-                {
-                    newNode = new ListNode(head->val);
-                    tail = newNode;
-                }
-                else
-                {
-                    tail->next=  new ListNode(head->val);
-                    tail=tail->next;
-                }
+
+            if (isDuplicate) {
+                prev->next = current->next;
+            } else {
+                prev = prev->next;
             }
-            head = head->next;
+
+            current = current->next;
         }
-        return newNode;
+
+        return dummy->next;
     }
 };
