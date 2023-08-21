3fd5fc3bd47b1b18502822ee686b360993a3f708
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-08-21 10:02:50 +0530

    Time: 5 ms (19.63%) | Memory: 7.5 MB (38.13%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
@@ -1,49 +1,23 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    int getSize(ListNode* root) {
-        if (root == nullptr) return 0;
-        return 1 + getSize(root->next);
+   ListNode* reverseBetween(ListNode* head, int x, int y) {
+    vector<int> list;
+    while (head != NULL) {
+        list.push_back(head->val);
+        head = head->next;
     }
-    
-    ListNode* reverseBetween(ListNode* head, int left, int right) {
-        if (head == nullptr || left >= right) return head;
-
-        int n = getSize(head);
-
-        // Create a dummy node to simplify handling of edge cases
-        ListNode dummy(0);
-        dummy.next = head;
-        ListNode* prev = &dummy;
-
-        // Move prev to the node before the left-th node
-        for (int i = 1; i < left; ++i) {
-            prev = prev->next;
+    reverse(list.begin() + x - 1, list.begin() + y); // Fixed range for reverse
+    ListNode* root = NULL;
+    ListNode* tail = NULL;
+    for (int i = 0; i < list.size(); i++) {
+        if (root == NULL) {
+            root = new ListNode(list[i]);
+            tail = root;
+        } else {
+            tail->next = new ListNode(list[i]);
+            tail = tail->next;
         }
-
-        // Reverse the sublist between left and right
-        ListNode* current = prev->next;
-        ListNode* tail = current;  // The last node of the reversed sublist
-        ListNode* next;
-
-        for (int i = left; i <= right; ++i) {
-            next = current->next;
-            current->next = prev->next;
-            prev->next = current;
-            current = next;
-        }
-
-        tail->next = current;  // Connect the reversed sublist with the rest of the list
-
-        return dummy.next;
     }
+    return root;
+   }
 };
