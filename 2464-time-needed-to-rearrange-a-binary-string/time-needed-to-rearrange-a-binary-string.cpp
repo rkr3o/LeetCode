1aa358b294e2898d4bf8c49c0ecf40e42287cfa6
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-08-25 19:16:37 +0530

    Time: 2 ms (88.23%) | Memory: 6.7 MB (24.65%) - LeetSync

--- 2464-time-needed-to-rearrange-a-binary-string/time-needed-to-rearrange-a-binary-string.cpp
+++ 2464-time-needed-to-rearrange-a-binary-string/time-needed-to-rearrange-a-binary-string.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     int secondsToRemoveOccurrences(string s) {
-        int cnt = 0 ;
-        while (true) {
-            bool f = false;
-            for (int i = 0; i < s.size() - 1; i++) {
-                if (s[i] == '0' && s[i + 1] == '1') {
-                    s[i] = '1';
-                    s[i + 1] = '0';
-                    i++;
-                    f = true;
-                }
-            }
-            if (!f) break;
-            else cnt++;
-        } 
-        return cnt;
+        int zeros = 0, seconds = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            zeros += s[i] == '0';
+            if (s[i] == '1' && zeros)
+                seconds = max(seconds + 1, zeros);
+        }
+        return seconds;
     }
 };
