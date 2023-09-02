104d3336c5998290a85a26992c5e5fd65ddbd600
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-09-02 13:21:21 +0530

    Time: 205 ms (75.73%) | Memory: 73.6 MB (92.88%) - LeetSync

--- 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.cpp
+++ 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.cpp
@@ -1,30 +1,28 @@
 class FindSumPairs {
 public:
-    vector<int> nums1;
-    vector<int> nums2;
-    unordered_map<int, int> freqMap;
-
+    vector<int> a;
+    vector<int> b;
+    unordered_map<int, int> mp;
     FindSumPairs(vector<int>& nums1, vector<int>& nums2) {
-        this->nums1 = nums1;
-        this->nums2 = nums2;
-        for (int num : nums2) {
-            freqMap[num]++;
+       a = nums1;
+       b = nums2;
+        for (auto x : nums2) {
+            mp[x]++;
         }
     }
-
     void add(int index, int val) {
-        int oldVal = nums2[index];
-        freqMap[oldVal]--;
-        nums2[index] += val;
-        freqMap[nums2[index]]++;
+        int oldVal = b[index];
+        mp[oldVal]--;
+        b[index] += val;
+        mp[b[index]]++;
     }
 
     int count(int tot) {
         int countPairs = 0;
-        for (int num : nums1) {
+        for (auto num : a) {
             int complement = tot - num;
-            if (freqMap.find(complement) != freqMap.end()) {
-                countPairs += freqMap[complement];
+            if (mp.find(complement) != mp.end()) {
+                countPairs += mp[complement];
             }
         }
         return countPairs;
