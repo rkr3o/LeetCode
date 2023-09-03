df5852c16a79018011dbe399e7230452c723f4b6
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-09-03 23:58:22 +0530

    Time: 0 ms (100.00%) | Memory: 6.5 MB (17.06%) - LeetSync

---
+++ 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <stack>
+#include <algorithm>
+
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        string res;
+        unordered_map<char, int> last;
+        for (int i = 0; i < s.size(); i++) {
+            last[s[i]] = i;
+        }
+        vector<char> vis(26, 0);
+        stack<char> st;
+        for (int i = 0; i < (int)s.size(); i++) {
+            char ch = s[i];
+            if (vis[ch - 'a']) continue;
+            while (!st.empty() && st.top() > ch && i < last[st.top()]) {
+                vis[st.top() - 'a'] = 0;
+                st.pop();
+            }
+            st.push(ch);
+            vis[ch - 'a'] = 1;
+        }
+        while (!st.empty()) {
+            res += st.top();
+            st.pop(); // Pop the elements from the stack
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
