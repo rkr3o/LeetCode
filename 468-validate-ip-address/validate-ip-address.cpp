d98a3e3006810dcec20dd3dc842f0115f70d1659
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-09-12 09:51:34 +0530

    Time: 0 ms (100.00%) | Memory: 6.1 MB (95.99%) - LeetSync

--- 468-validate-ip-address/validate-ip-address.cpp
+++ 468-validate-ip-address/validate-ip-address.cpp
@@ -1,87 +1,95 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 class Solution {
 public:
-    string checkIPv4(string IP){
-        int i = 0, j = 0, count = 0;
-        while(j < IP.length()){
-            if(IP[j] == '.'){
-                count++;
-                string s = IP.substr(i, j - i);
-                if(s.length() == 0)
-                    return "Neither";
-                if(s[0] == '0' and s.length()!= 1)
-                    return "Neither";
-                if(stoi(s) > 255)
-                    return "Neither";
-                i = j + 1;
-                j = j + 1;
-                if(count==3)
-                    break;
-            }
-            else
-                j = j + 1;
-        }
-        string s = IP.substr(i, IP.length() - i);
-        if(s.length() == 0)
-            return "Neither";
-        if(s[0] == '0' and s.length()!= 1)
-                    return "Neither";
-        if(stoi(s) > 255)
-                    return "Neither";
-        return "IPv4";
-    }
-    
-    string checkIPv6(string IP){
-        int i = 0, j = 0, count = 0;
-        while(j < IP.length()){
-            if(IP[j] == ':'){
-                count++;
-                string s = IP.substr(i, j - i);
-                if(s.length() < 1 or s.length() > 4)
-                    return "Neither";
-                i = j + 1;
-                j = j + 1;
-                if(count==7)
-                    break;
-            }
-            else
-                j = j + 1;
-        }
-        string s = IP.substr(i, IP.length() - j);
-        
-        if(s.length() == 0)
-            return "Neither";
-        if(s.length() < 1 or s.length() > 4)
-                    return "Neither";
-        return "IPv6";
-    }
     string validIPAddress(string IP) {
-         int dot = 0, colon = 0;
-        for(int  i = 0; i < IP.length(); i++){
-            if(IP[i] == '.')
-                dot++;
-            if(IP[i] == ':')
-                colon++;
+        if (isValidIPv4(IP)) {
+            return "IPv4";
+        } else if (isValidIPv6(IP)) {
+            return "IPv6";
         }
-        
-        if(dot == 3 and colon == 0 and IP.length() <= 15){
-            for(int i = 0; i < IP.length(); i++){
-                if(isalpha(IP[i]))
-                    return "Neither";
-            }
-            return checkIPv4(IP);
-        }
-        else if(colon == 7 and dot == 0){
-            for(int i = 0; i < IP.length(); i++){
-                if(IP[i] >= 'G' and IP[i] <= 'Z')
-                    return "Neither";
-                if(IP[i] >= 'g' and IP[i] <= 'z')
-                    return "Neither";
-            }
-            return checkIPv6(IP);
-        }
-        else
-            return "Neither";
+        return "Neither";
     }
-}; 
 
-    
+private:
+    bool isValidIPv4(const string& IP) {
+        if (IP.length() > 15) {
+            return false;
+        }
+
+        int count = 0;
+        size_t start = 0;
+        size_t end = IP.find('.');
+        while (end != string::npos) {
+            count++;
+            string s = IP.substr(start, end - start);
+            if (!isValidIPv4Part(s)) {
+                return false;
+            }
+            start = end + 1;
+            end = IP.find('.', start);
+        }
+
+        if (count != 3) {
+            return false;
+        }
+
+        string s = IP.substr(start);
+        return isValidIPv4Part(s);
+    }
+
+    bool isValidIPv4Part(const string& part) {
+        if (part.empty() || part.size() > 3 || (part.size() > 1 && part[0] == '0')) {
+            return false;
+        }
+        int num = 0;
+        for (char c : part) {
+            if (!isdigit(c)) {
+                return false;
+            }
+            num = num * 10 + (c - '0');
+        }
+        return num >= 0 && num <= 255;
+    }
+
+    bool isValidIPv6(const string& IP) {
+        if (IP.length() > 39) {
+            return false;
+        }
+
+        int count = 0;
+        size_t start = 0;
+        size_t end = IP.find(':');
+        while (end != string::npos) {
+            count++;
+            string s = IP.substr(start, end - start);
+            if (!isValidIPv6Part(s)) {
+                return false;
+            }
+            start = end + 1;
+            end = IP.find(':', start);
+        }
+
+        if (count != 7) {
+            return false;
+        }
+
+        string s = IP.substr(start);
+        return isValidIPv6Part(s);
+    }
+
+    bool isValidIPv6Part(const string& part) {
+        if (part.empty() || part.size() > 4) {
+            return false;
+        }
+        for (char c : part) {
+            if (!isxdigit(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
