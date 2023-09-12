5af2bbabd44d29aef6ae5e110508a7dde432aa07
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-09-12 12:39:41 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (78.55%) - LeetSync

--- 468-validate-ip-address/validate-ip-address.cpp
+++ 468-validate-ip-address/validate-ip-address.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
-    string validIPAddress(string IP) {
-        if(count(IP.begin(),IP.end(),'.')==3)
+    string validIPAddress(string IP) 
+    {
+        if(count(IP.begin(), IP.end(), '.') == 3)
         {
-            if(IP[IP.size()-1]=='.')return "Neither";
-            stringstream ss(IP);
+            if(IP[IP.size() - 1] == '.') return "Neither";
+            istringstream ss(IP);  // Corrected 'isstringstream' to 'istringstream'
             string token;
-            while(getline(ss,token,'.'))
+            while(getline(ss, token, '.'))
             {
-                if(token.size()==0 || token.size()>3)return "Neither";
-                if(token[0]=='0' and token.size()!=1)return "Neither";
+                if(token.size() == 0 || token.size() > 3) return "Neither";
+                if(token[0] == '0' && token.size() != 1) return "Neither";
                 for(auto &x : token)
                 {
-                    if(!isdigit(x))return "Neither";
+                    if(!isdigit(x)) return "Neither";
                 }
-                if(stoi(token)>255)return "Neither";
+                if(stoi(token) > 255) return "Neither";
             }
-             return "IPv4";
+            return "IPv4";
         }
-        else  if(count(IP.begin(),IP.end(),':')==7)
+        else if(count(IP.begin(), IP.end(), ':') == 7)
         {
-            if(IP[IP.size()-1]==':')return "Neither";
+            if(IP[IP.size() - 1] == ':') return "Neither";
             stringstream ss(IP);
             string token;
-            while(getline(ss,token,':'))
+            while(getline(ss, token, ':'))
             {
-                if(token.size()==0 || token.size()>4)return "Neither";
+                if(token.size() == 0 || token.size() > 4) return "Neither";
                 for(auto x : token)
                 {
-                    if(!isxdigit(x))return "Neither";
+                    if(!isxdigit(x)) return "Neither";
                 }
             }
             return "IPv6";
