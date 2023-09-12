23b1f735e2d60e5a25827d112b318153d29a8538
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-09-12 10:37:36 +0530

    Time: 3 ms (53.70%) | Memory: 6.3 MB (78.55%) - LeetSync

--- 468-validate-ip-address/validate-ip-address.cpp
+++ 468-validate-ip-address/validate-ip-address.cpp
@@ -1,95 +1,38 @@
-#include <iostream>
-#include <string>
-
-using namespace std;
-
 class Solution {
 public:
     string validIPAddress(string IP) {
-        if (isValidIPv4(IP)) {
-            return "IPv4";
-        } else if (isValidIPv6(IP)) {
+        if(count(IP.begin(),IP.end(),'.')==3)
+        {
+            if(IP[IP.size()-1]=='.')return "Neither";
+            stringstream ss(IP);
+            string token;
+            while(getline(ss,token,'.'))
+            {
+                if(token.size()==0 || token.size()>3)return "Neither";
+                if(token[0]=='0' and token.size()!=1)return "Neither";
+                for(auto &x : token)
+                {
+                    if(!isdigit(x))return "Neither";
+                }
+                if(stoi(token)>255)return "Neither";
+            }
+             return "IPv4";
+        }
+        else  if(count(IP.begin(),IP.end(),':')==7)
+        {
+            if(IP[IP.size()-1]==':')return "Neither";
+            stringstream ss(IP);
+            string token;
+            while(getline(ss,token,':'))
+            {
+                if(token.size()==0 || token.size()>4)return "Neither";
+                for(auto x : token)
+                {
+                    if(!isxdigit(x))return "Neither";
+                }
+            }
             return "IPv6";
         }
         return "Neither";
-    }
-
-private:
-    bool isValidIPv4(const string& IP) {
-        if (IP.length() > 15) {
-            return false;
-        }
-
-        int count = 0;
-        size_t start = 0;
-        size_t end = IP.find('.');
-        while (end != string::npos) {
-            count++;
-            string s = IP.substr(start, end - start);
-            if (!isValidIPv4Part(s)) {
-                return false;
-            }
-            start = end + 1;
-            end = IP.find('.', start);
-        }
-
-        if (count != 3) {
-            return false;
-        }
-
-        string s = IP.substr(start);
-        return isValidIPv4Part(s);
-    }
-
-    bool isValidIPv4Part(const string& part) {
-        if (part.empty() || part.size() > 3 || (part.size() > 1 && part[0] == '0')) {
-            return false;
-        }
-        int num = 0;
-        for (char c : part) {
-            if (!isdigit(c)) {
-                return false;
-            }
-            num = num * 10 + (c - '0');
-        }
-        return num >= 0 && num <= 255;
-    }
-
-    bool isValidIPv6(const string& IP) {
-        if (IP.length() > 39) {
-            return false;
-        }
-
-        int count = 0;
-        size_t start = 0;
-        size_t end = IP.find(':');
-        while (end != string::npos) {
-            count++;
-            string s = IP.substr(start, end - start);
-            if (!isValidIPv6Part(s)) {
-                return false;
-            }
-            start = end + 1;
-            end = IP.find(':', start);
-        }
-
-        if (count != 7) {
-            return false;
-        }
-
-        string s = IP.substr(start);
-        return isValidIPv6Part(s);
-    }
-
-    bool isValidIPv6Part(const string& part) {
-        if (part.empty() || part.size() > 4) {
-            return false;
-        }
-        for (char c : part) {
-            if (!isxdigit(c)) {
-                return false;
-            }
-        }
-        return true;
     }
 };
