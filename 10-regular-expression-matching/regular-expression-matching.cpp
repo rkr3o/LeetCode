b78c9c625d4f3d2b765daf3cebcb122a7c78c689
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-09-20 01:57:08 +0530

    Time: 5 ms (47.26%) | Memory: 7.3 MB (21.17%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,24 +1,30 @@
 class Solution {
-
-   int dp[1005][1005];
-   bool Solve(string& s,string& p,int i,int j)
-   {
-       if(j==p.length())
-            return i==s.length();
-       if(dp[i][j]!=-1)
-            return dp[i][j];
-       bool first=(i<s.length() && (s[i]==p[j] || p[j]=='.'));
-       if(j+1<p.length() && p[j+1]=='*')
-       {
-            return dp[i][j]= Solve(s,p,i,j+2) || (first && Solve(s,p,i+1,j));
-       }
-       else
-           return dp[i][j]=(first && Solve(s,p,i+1,j+1));
-   }
 public:
-    bool isMatch(string s, string p) 
-    {
-         memset(dp,-1,sizeof(dp));
-         return Solve(s,p,0,0);
+    bool solve(int n,int m,string& s,string& p,vector<vector<int>>&dp){
+        // cout<<n<<" "<<m<<'\n';
+        if(n<0 && m<0) return 1;
+        if(n<0){
+            while(m>=0){
+                if(p[m]!='*') return 0;
+                m-=2; // _a* -> m at * so it will delete a as well so m-=2
+            }
+            return 1;
+        }
+        if(m<0) return 0;
+        if(dp[n][m]!=-1) return dp[n][m];
+        if(p[m]=='.' || s[n]==p[m]) return dp[n][m]=solve(n-1,m-1,s,p,dp);
+        if(p[m]=='*'){
+            dp[n][m]=solve(n,m-2,s,p,dp); // 0 occurence of last characters
+            if((p[m-1]=='.' || p[m-1]==s[n])) dp[n][m]|=solve(n-1,m,s,p,dp); // take occurence of last character 
+            return dp[n][m];
+        }
+        return dp[n][m]=0;
+    }
+    
+    // here c* => "cccccccc" (* means 0/multiple occurence of prev char)
+    bool isMatch(string s, string p){
+        int n=s.size(),m=p.size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return solve(n-1,m-1,s,p,dp);
     }
 };
