f18a057b8f4e396a3749fdb6d8e28b5b1185665c
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-10-26 18:17:37 +0530

    Time: 23 ms (66.38%) | Memory: 26.8 MB (76.84%) - LeetSync

--- 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.cpp
@@ -1,23 +1,36 @@
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
-        int pre = -1;
-        int suff=-1;
-        vector<int>nums2=nums;
-        sort(nums2.begin(),nums2.end());
         int n = nums.size();
-        for(int i = 0 ; i < n ; i++)
-        {
-            if(nums[i]!=nums2[i] and pre==-1)
-            {
-                pre = i;
-            }
-            else if(nums[i]!=nums2[i] and pre!=-1)
-            {
-                suff=i;
+        if (n <= 1) return 0;
+        int left = -1;
+        int right = -1;
+        // Find the first element that is smaller than its left neighbor
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < nums[i - 1]) {
+                left = i - 1;
+                break;
             }
         }
-        if(pre!=-1 and suff!=-1)return suff-pre+1;
-        return 0;
+        // If left is still -1, the array is already sorted
+        if (left == -1) return 0;
+        // Find the last element that is smaller than its right neighbor
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] > nums[i + 1]) {
+                right = i + 1;
+                break;
+            }
+        }
+        // Find the minimum and maximum values within the unsorted subarray
+        int min_val = *min_element(nums.begin() + left, nums.begin() + right + 1);
+        int max_val = *max_element(nums.begin() + left, nums.begin() + right + 1);
+        // Expand the left and right boundaries if needed
+        while (left > 0 && nums[left - 1] > min_val) {
+            left--;
+        }
+        while (right < n - 1 && nums[right + 1] < max_val) {
+            right++;
+        }
+        return right - left + 1;
     }
 };
