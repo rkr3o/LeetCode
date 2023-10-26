e9e9a83cd06192045a1abd2ead605b5128849409
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-10-27 00:32:40 +0530

    Time: 372 ms (55.03%) | Memory: 162.4 MB (55.65%) - LeetSync

--- 2564-most-profitable-path-in-a-tree/most-profitable-path-in-a-tree.cpp
+++ 2564-most-profitable-path-in-a-tree/most-profitable-path-in-a-tree.cpp
@@ -1,41 +1,43 @@
 class Solution {
 public:
-    int cross_time(int cur,int p,vector<int>v[],vector<int>&c,int bob){
-        if(cur==bob) return c[cur]=0;
-        for(auto i:v[cur]){
-            if(i==p) continue;
-            int time=cross_time(i,cur,v,c,bob);
-            if(time!=-1) return c[cur]=1+time;
+    int crossTime(int cur , int bob,  int prev , vector<int>adj[] , vector<int>&time)
+    {
+        if(cur==bob)return time[cur]=0;
+        for(auto x : adj[cur])
+        {
+            //overcome from cycle
+            if(x==prev)continue;
+            int timeReach = crossTime(x,bob,cur,adj,time);
+            if(timeReach!=-1) return time[cur]=1+timeReach;
         }
         return -1;
     }
-    
-    void dfs(int alice,int p,vector<int>&a,vector<int>&c,vector<int>v[],int
-             cur_sum,int cur_time,int&ans){
-        // cout<<alice<<" "<<p<<" "<<cur_sum<<" "<<cur_time<<" "<<ans<<"   ";
-        if(cur_time<c[alice]) cur_sum+=a[alice];
-        else if(cur_time==c[alice]) cur_sum+=a[alice]/2;
-        // cout<<cur_sum<<" "<<cur_time<<"\n";
-        if(v[alice].size()==1 && alice) ans=max(ans,cur_sum);
-        for(auto i:v[alice]){
-            if(i==p) continue;
-            dfs(i,alice,a,c,v,cur_sum,cur_time+1,ans);
+    void dfs(int alice , int prev , vector<int>&amount , vector<int>&time , vector<int>adj[] , int curSum , int curTime , int &ans)
+    {
+        if(curTime < time[alice])curSum+=amount[alice];
+        else if(curTime==time[alice])curSum+=amount[alice]/2;
+        if(adj[alice].size()==1 and alice) ans = max(ans,curSum);
+        for(auto x : adj[alice])
+        {
+            if(x==prev)continue;
+            dfs(x,alice,amount,time,adj,curSum,curTime+1,ans);
         }
-        return;
+        return ;
     }
-    
-    int mostProfitablePath(vector<vector<int>>& e,int b,vector<int>& a){
-        int n=a.size();
-        vector<int>v[n];
-        for(auto i:e){
-            v[i[0]].push_back(i[1]);
-            v[i[1]].push_back(i[0]);
-        }
-        vector<int>cross_time_bob(n,1e6);    
-        cross_time(0,-1,v,cross_time_bob,b);
-        // for(auto i:cross_time_bob) cout<<i<<' '; cout<<'\n';
-        int ans=-2e9;
-        dfs(0,-1,a,cross_time_bob,v,0,0,ans);
-        return ans;
+    int mostProfitablePath(vector<vector<int>>& edges, int bob, vector<int>& amount) {
+         int n = edges.size();
+         vector<int>adj[n+1];
+         for(auto x: edges)
+         {
+             int u = x[0];
+             int v = x[1];
+             adj[u].push_back(v);
+             adj[v].push_back(u);
+         }
+         vector<int>time(n+1,1e6);
+         crossTime(0,bob,-1,adj,time);
+         int ans = -2e9;
+         dfs(0,-1,amount,time,adj,0,0,ans);
+         return ans;
     }
 };
