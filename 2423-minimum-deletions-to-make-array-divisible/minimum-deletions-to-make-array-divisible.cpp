d5f06c198c662222dd9756a5a142d587a122a7d1
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-10-26 16:36:14 +0530

    Time: 113 ms (77.11%) | Memory: 88.5 MB (46.83%) - LeetSync

--- 2423-minimum-deletions-to-make-array-divisible/minimum-deletions-to-make-array-divisible.cpp
+++ 2423-minimum-deletions-to-make-array-divisible/minimum-deletions-to-make-array-divisible.cpp
@@ -17,3 +17,8 @@ public:
         return (cnt==n)?-1:cnt;
     }
 };
+/*
+Little bit of maths here we have to think how can we check whole numsDivide array is divisible by a number without traversing whole numsDivide array and checking one by one each element is divisibel by nums[i] or not . Becuase this will give tle --> O(n^2)
+So we know a property is there by which a set of elemnts can be checked if they are divisible by some number or not and that is GCD.
+Since gcd is common divisor of all the numbers in set if gcd is divible by a number it means all the numbers will be divisible by that number . So we can find the gcd of whole numsDivide array and check divisiblity by nums array in sorted way one by one .
+*/
