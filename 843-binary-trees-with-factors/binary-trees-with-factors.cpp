08866c050063fbd55ec109b516cced25f85246f8
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-10-27 20:25:23 +0530

    Time: 59 ms (42.91%) | Memory: 10.6 MB (31.10%) - LeetSync

--- 843-binary-trees-with-factors/binary-trees-with-factors.cpp
+++ 843-binary-trees-with-factors/binary-trees-with-factors.cpp
@@ -4,7 +4,7 @@ public:
     long long solve(int target, vector<int>& arr, unordered_set<int>& st, unordered_map<int,long long>&mp) {
         if (mp.find(target) != mp.end()) 
              return mp[target];
-        long long ways = 1;  // Initialize ways to 0, not 1
+        long long ways = 1;  
         for (int i = 0; i < arr.size(); i++) {
             if (target % arr[i] == 0 && st.find(target / arr[i]) != st.end()) {
                 ways += solve(arr[i], arr, st, mp) * solve(target / arr[i], arr, st, mp);
@@ -18,9 +18,20 @@ public:
         long long cnt = 0;
         int n = arr.size();
         unordered_map<int,long long>mp;
-        for (int i = 0; i < n; i++) {
-            cnt += solve(arr[i], arr, st, mp);
-            cnt %= mod;
+        
+        for (int i = 0; i < n; i++) 
+        {
+            if(mp.find(arr[i])!=mp.end())
+            {
+                cnt += mp[arr[i]];
+                cnt %= mod;
+            }
+            else
+            {
+                cnt += solve(arr[i], arr, st, mp);
+                cnt %= mod;
+            }
+
         }
         return static_cast<int>(cnt);
     }
