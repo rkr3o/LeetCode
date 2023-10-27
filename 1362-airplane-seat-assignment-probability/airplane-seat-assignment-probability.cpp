477bb90080b403b5041be519cea4865c7350e3b6
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-10-27 23:18:32 +0530

    Time: 17 ms (5.28%) | Memory: 9.5 MB (5.67%) - LeetSync

--- 1362-airplane-seat-assignment-probability/airplane-seat-assignment-probability.cpp
+++ 1362-airplane-seat-assignment-probability/airplane-seat-assignment-probability.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
+
     double nthPersonGetsNthSeat(int n) {
-        if(n==1)return 1.0;
-        else return 0.5;
+        if (n == 1) {
+            return 1.0; 
+        }
+        return 1.0 / n + (n - 2.0) / n * nthPersonGetsNthSeat(n - 1);
+
     }
 };
+/*
+there will be two cases
+if n=1 -> there's only one passenger, they will always get their own seat
+in other case
+total probability = Probability for the nth person is 1/n for getting the first seat and (n-2)/n for getting the others
+and
+Probability of getting other seats is calculated recursively for n-1 passengers
+*/
