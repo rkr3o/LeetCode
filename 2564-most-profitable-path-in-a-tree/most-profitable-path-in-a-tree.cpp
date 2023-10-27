06bbad773002c40d70ff2c89884349596a2b2b14
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-10-28 00:53:38 +0530

    Time: 392 ms (45.32%) | Memory: 162.2 MB (56.34%) - LeetSync

--- 2564-most-profitable-path-in-a-tree/most-profitable-path-in-a-tree.cpp
+++ 2564-most-profitable-path-in-a-tree/most-profitable-path-in-a-tree.cpp
@@ -1,43 +1,40 @@
 class Solution {
 public:
-    int crossTime(int cur , int bob,  int prev , vector<int>adj[] , vector<int>&time)
-    {
-        if(cur==bob)return time[cur]=0;
-        for(auto x : adj[cur])
-        {
-            //overcome from cycle
-            if(x==prev)continue;
-            int timeReach = crossTime(x,bob,cur,adj,time);
-            if(timeReach!=-1) return time[cur]=1+timeReach;
+    int getTime(int src , int par , int bob , vector<int>& time , vector<int> adj[]) {
+        if (src == bob) return time[src] = 0;
+        for (auto x : adj[src]) {
+            if (x == par) continue;
+            int t = getTime(x, src, bob, time, adj);
+            if (t != -1) return time[src] = 1 + t;
         }
         return -1;
     }
-    void dfs(int alice , int prev , vector<int>&amount , vector<int>&time , vector<int>adj[] , int curSum , int curTime , int &ans)
-    {
-        if(curTime < time[alice])curSum+=amount[alice];
-        else if(curTime==time[alice])curSum+=amount[alice]/2;
-        if(adj[alice].size()==1 and alice) ans = max(ans,curSum);
-        for(auto x : adj[alice])
-        {
-            if(x==prev)continue;
-            dfs(x,alice,amount,time,adj,curSum,curTime+1,ans);
+    
+    void dfs(int alice , int par , vector<int>& time , vector<int>& amt , vector<int> adj[] , int curSum , int curTime , int &ans) {
+        if (curTime < time[alice]) curSum += amt[alice];
+        else if (curTime == time[alice]) curSum += amt[alice] / 2;
+
+        if (adj[alice].size() == 1 && alice) ans = max(ans, curSum);
+
+        for (auto x : adj[alice]) {
+            if (x == par) continue;
+            dfs(x, alice, time, amt, adj, curSum, curTime + 1, ans);
         }
-        return ;
     }
+
     int mostProfitablePath(vector<vector<int>>& edges, int bob, vector<int>& amount) {
-         int n = edges.size();
-         vector<int>adj[n+1];
-         for(auto x: edges)
-         {
-             int u = x[0];
-             int v = x[1];
-             adj[u].push_back(v);
-             adj[v].push_back(u);
-         }
-         vector<int>time(n+1,1e6);
-         crossTime(0,bob,-1,adj,time);
-         int ans = -2e9;
-         dfs(0,-1,amount,time,adj,0,0,ans);
-         return ans;
+        int n = amount.size();
+        vector<int> adj[n+1];
+        for (auto x : edges) {
+            int u = x[0];
+            int v = x[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        vector<int> time(n+1, 1e6);
+        getTime(0, -1, bob, time, adj);
+        int ans = -2e9;
+        dfs(0, -1, time, amount, adj, 0, 0, ans);
+        return ans;
     }
 };
