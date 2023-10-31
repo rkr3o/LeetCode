75bccf310944b4331ce40bcc542895d11bdfa76b
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-10-31 13:13:22 +0530

    Time: 106 ms (14.64%) | Memory: 53.7 MB (69.90%) - LeetSync

---
+++ 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxCoins(vector<int>& piles) {
+        sort(piles.begin(),piles.end());
+        int size = piles.size()/3;
+        int val = 0 ;
+        for(int i = size ; i < piles.size() ; i+=2)
+           val+=piles[i];
+        return val;
+    }
+};
