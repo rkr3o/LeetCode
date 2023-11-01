3c220d81faec3314b7ae7d071606a77ef60861aa
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-11-01 16:04:43 +0530

    Time: 65 ms (33.27%) | Memory: 27.1 MB (8.88%) - LeetSync

--- 149-max-points-on-a-line/max-points-on-a-line.cpp
+++ 149-max-points-on-a-line/max-points-on-a-line.cpp
@@ -2,24 +2,35 @@ class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
         int n = points.size();
-        if(n==1)return 1;
         int res = 0;
-        for (int i = 0; i < n-1 ; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int cnt = 2;
+
+        for (int i = 0; i < n; i++) {
+            unordered_map<string, int> slopeCount;
+            int samePoint = 0; // Count of points with the same coordinates as points[i]
+
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+
                 int dx = points[j][0] - points[i][0];
                 int dy = points[j][1] - points[i][1];
-                for (int k = j+1; k < n; k++) 
-                {
-                    int dx_ = points[k][0] - points[i][0];
-                    int dy_ = points[k][1] - points[i][1];
-                    if (dx * dy_ == dx_ * dy) {
-                        cnt++;
-                    } 
+
+                if (dx == 0 && dy == 0) {
+                    samePoint++;
+                } else {
+                    int gcd = __gcd(dx, dy);
+                    string slope = to_string(dx / gcd) + "_" + to_string(dy / gcd);
+                    slopeCount[slope]++;
                 }
-                res = max(res, cnt);
             }
+
+            int maxInLine = samePoint;
+            for (const auto& kvp : slopeCount) {
+                maxInLine = max(maxInLine, kvp.second + samePoint);
+            }
+
+            res = max(res, maxInLine);
         }
-        return res;
+
+        return res + 1; // Add 1 to include the point itself.
     }
 };
