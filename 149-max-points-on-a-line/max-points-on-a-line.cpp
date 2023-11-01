922d91d391d5a9ca49b7638e10ca471406473db7
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-11-01 15:54:56 +0530

    Time: 81 ms (29.49%) | Memory: 7.7 MB (95.19%) - LeetSync

--- 149-max-points-on-a-line/max-points-on-a-line.cpp
+++ 149-max-points-on-a-line/max-points-on-a-line.cpp
@@ -9,14 +9,13 @@ public:
                 int cnt = 2;
                 int dx = points[j][0] - points[i][0];
                 int dy = points[j][1] - points[i][1];
-                for (int k = 0; k < n; k++) {
-                    if (k != i && k != j) {
-                        int dx_ = points[k][0] - points[i][0];
-                        int dy_ = points[k][1] - points[i][1];
-                        if (dx * dy_ == dx_ * dy) {
-                            cnt++;
-                        }
-                    }
+                for (int k = j+1; k < n; k++) 
+                {
+                    int dx_ = points[k][0] - points[i][0];
+                    int dy_ = points[k][1] - points[i][1];
+                    if (dx * dy_ == dx_ * dy) {
+                        cnt++;
+                    } 
                 }
                 res = max(res, cnt);
             }
