42d01416a61acee2e00fca24f2d5bd20011cf1f3
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-11-02 21:38:25 +0530

    Time: 5 ms (61.11%) | Memory: 6.8 MB (72.86%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int countSubstrings(string s) {
         int n = s.size();
+        //dp[i] = whether the substring ending at index i is a palindrome or not.
+
         int count = 0;
         vector<int> dp(n, 0);
         for (int i = 0; i < n; i++) {
