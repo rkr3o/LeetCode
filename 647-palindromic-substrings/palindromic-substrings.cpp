0b8ed1ccab978ae745647c812d8cb9665a5d1b8f
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-11-02 21:38:04 +0530

    Time: 10 ms (43.23%) | Memory: 6.8 MB (74.32%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
-    int dp[1000];
-    bool isPalindrome(string s) {
-       string t = s;
-       reverse(t.begin(),t.end());
-       return s==t;
-    }
-    int countPalindromicSubstrings(int ind, string &s) {
-        if (ind >= s.size()) {
-            return 0;
-        }
-        if(dp[ind]!=-1)return dp[ind];
-        int cnt = 0;
-        string temp = "";
-        for (int i = ind; i < s.size(); i++) {
-            temp += s[i];
-            if (isPalindrome(temp)) {
-                cnt++;  // Increment the count when a palindrome is found.
-            }
-        }
-        // Recursively count palindromic substrings starting from the next index.
-        cnt += countPalindromicSubstrings(ind + 1, s);
-        return dp[ind]= cnt;
-    }
     int countSubstrings(string s) {
         int n = s.size();
-        memset(dp,-1,sizeof(dp));
-        return countPalindromicSubstrings(0, s);
+        int count = 0;
+        vector<int> dp(n, 0);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (s[i] == s[j] && (i - j < 2 || dp[j + 1])) {
+                    dp[j] = 1;
+                    count++;
+                } else {
+                    dp[j] = 0;
+                }
+            }
+        }
+
+        return count;
     }
 };
