f65a9bd37da635b16b74d2a3425eab3073062fa6
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-11-05 11:50:28 +0530

    Time: 100 ms (21.76%) | Memory: 68.6 MB (16.20%) - LeetSync

--- 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.cpp
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
     int getWinner(vector<int>& arr, int k) {
-        int maxima = arr[0];
-        int cnt= 0 ;
-        for(int i = 1 ; i < arr.size() ;i++)
-        {
-            if(arr[i]>maxima)
-            {
-                maxima = arr[i];
-                cnt=0;
-            }
-            cnt++;
-            if(cnt==k) break;
+        int maxElement = arr[0];
+        queue<int> queue;
+        for (int i = 1; i < arr.size(); i++) {
+            maxElement = max(maxElement, arr[i]);
+            queue.push(arr[i]);
         }
-        return maxima;
+        
+        int curr = arr[0];
+        int winstreak = 0;
+        
+        while (!queue.empty()) {
+            int opponent = queue.front();
+            queue.pop();
+            
+            if (curr > opponent) {
+                queue.push(opponent);
+                winstreak++;
+            } else {
+                queue.push(curr);
+                curr = opponent;
+                winstreak = 1;
+            }
+            
+            if (winstreak == k || curr == maxElement) {
+                return curr;
+            }
+        }
+        
+        return -1;
     }
 };
