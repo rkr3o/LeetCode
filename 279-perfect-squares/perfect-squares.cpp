ff033ff64803ffd1033bec4bd76444784e11289d
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-11-06 16:32:07 +0530

    Time: 59 ms (92.38%) | Memory: 6.7 MB (92.76%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    long long solve(int ind , int t , vector<long long>&arr , vector<vector<int>>&dp)
-    {
-        if(ind>=arr.size() || t<0)
-          return INT_MAX;
-        if(t==0)return 0;
-        if(dp[ind][t]!=-1)return dp[ind][t];
-        long long  notPick = solve(ind+1,t,arr,dp);
-        long long pick = INT_MAX;
-        if(arr[ind]<=t)
-        {
-             pick = 1+solve(ind,t-arr[ind],arr,dp);
+     int dp[10000];
+     int solve(int t) {
+        if (t == 0) return 0;
+        if (dp[t] != -1) return dp[t];
+
+        int ans = INT_MAX;
+        for (int i = 1; i*i <= t; i++) {
+            int val = 1 + solve(t - i * i);
+            ans = min(ans, val);
         }
-        return dp[ind][t]=min(pick,notPick);
+        return dp[t] = ans;
     }
-    int numSquares(int t) 
-    {
-        vector<long long>arr;
-        for (int i = 1; i <= 100; ++i)
-        {
-            arr.push_back(i*i);
-        }
-        int n = arr.size();
-        vector<vector<int>>dp(n,vector<int>(t+1,-1));
-        return solve(0,t,arr,dp);
+
+    int numSquares(int n) {
+       memset(dp,-1,sizeof(dp));
+        return solve(n);
     }
 };
