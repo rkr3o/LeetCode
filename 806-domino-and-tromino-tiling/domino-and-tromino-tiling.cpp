49ae90dff1203a0965442f3915a867a006b06b53
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-11-06 11:07:51 +0530

    Time: 2 ms (53.21%) | Memory: 6.6 MB (58.31%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     const int mod = 1e9+7;
-    int dp[1000];
+    int dp[1001];
     int solve(int n)
     {
         if(n<=2)return n ;
@@ -10,7 +10,14 @@ public:
         return dp[n] = (2*solve(n-1)%mod+solve(n-3)%mod)%mod;
     }
     int numTilings(int n) {
-        memset(dp,-1,sizeof(dp));
-        return solve(n);
+        memset(dp,0,sizeof(dp));
+        dp[1]=1;
+        dp[2]=2;
+        dp[3]=5;
+        for(int i = 4 ; i <= n ; i++)
+        {
+            dp[i]=(2*dp[i-1]%mod+dp[i-3]%mod)%mod;
+        }
+        return dp[n];
     }
 };
