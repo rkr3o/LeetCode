f9730464b23ebd76165f11bd7d6bbb793ea8903a
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-11-14 15:36:36 +0530

    Time: 256 ms (72.60%) | Memory: 13.5 MB (56.62%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        unordered_set<char>letter;
-        int cnt = 0 ;
-        for(auto x : s)letter.insert(x);
-        for(auto x : letter)
-        {
-            int left = -1;
-            int right = -1;
-            for(int i = 0 ; i < s.size() ; i++)
-            {
-                if(s[i]==x)
-                {
-                    if(left==-1)left=i;
-                    right=i;
-                }
-            }
-            unordered_set<char>st;
-            for(int mid = left+1; mid < right ; mid++)
-            {
+        unordered_set<char> letter;
+        int cnt = 0;
+        int n = s.size();
+        for (auto x : s) letter.insert(x);
+        vector<int> left(26, -1), right(26, -1);
+        for (int i = 0; i < n; i++) {
+            if (left[s[i] - 'a'] == -1) left[s[i] - 'a'] = i;
+            right[s[i] - 'a'] = i;  // Fix the typo here
+        }
+        for (auto x : letter) {
+            int l = left[x - 'a'];
+            int r = right[x - 'a'];
+            unordered_set<char> st;
+            for (int mid = l + 1; mid < r; mid++) {
                 st.insert(s[mid]);
             }
-            cnt+=st.size();
+            cnt += st.size();
         }
         return cnt;
     }
