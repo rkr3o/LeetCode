505754518b5ca8535adba43430d29cf730dc7a33
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-11-17 01:28:23 +0530

    Time: 750 ms (26.73%) | Memory: 170 MB (59.68%) - LeetSync

--- 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.cpp
@@ -1,48 +1,27 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+ 
 class Solution {
 public:
-    void merge(int low, int mid, int high, vector<pair<int,int>>& v, vector<int>& count) {
-        vector<pair<int, int>> temp;
-        int left = low;
-        int right = mid + 1;
-        int rightCount = 0; // Count of smaller elements on the right side
-
-        while (left <= mid) 
-        {
-            while (right <= high && v[right].second < v[left].second) {
-                rightCount++; // Count smaller elements on the right
-                temp.push_back(v[right++]);
-            }
-            count[v[left].first] += rightCount;
-            temp.push_back(v[left++]);
-        }
-
-        while (right <= high) {
-            temp.push_back(v[right++]);
-        }
-
-        for (int i = low; i <= high; i++) {
-            v[i] = temp[i - low];
-        }
-    }
-
-    void mergeSort(int low, int high, vector<pair<int,int>>& nums, vector<int>& ans) {
-        if (low >= high) {
-            return;
-        }
-        int mid = (low + high) / 2;
-        mergeSort(low, mid, nums, ans);
-        mergeSort(mid + 1, high, nums, ans);
-        merge(low, mid, high, nums, ans);
-    }
-
     vector<int> countSmaller(vector<int>& nums) {
         int n = nums.size();
-        vector<pair<int, int>> v;
-        for (int i = 0; i < n; i++) {
-            v.push_back({ i, nums[i] });
+        vector<int> counts(n, 0);
+        ordered_set s;
+
+        for (int i = n - 1; i >= 0; --i) {
+            // counts[i] is the number of elements smaller than nums[i] to its right
+            counts[i] = s.order_of_key(nums[i]);
+            // Insert nums[i] into the ordered_set
+            s.insert(nums[i]);
         }
-        vector<int> ans(n, 0);
-        mergeSort(0, n - 1, v, ans);
-        return ans;
+
+        return counts;
     }
 };
