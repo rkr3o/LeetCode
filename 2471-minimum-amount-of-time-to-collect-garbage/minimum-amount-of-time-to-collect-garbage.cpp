7d2ff2f896ed51c294395374a2b666bd6d58317e
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-11-20 12:01:31 +0530

    Time: 194 ms (14.69%) | Memory: 103.9 MB (11.25%) - LeetSync

--- 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.cpp
+++ 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.cpp
@@ -7,25 +7,23 @@ public:
         for (int i = 1; i <= travel.size(); i++) {
             pre[i] = travel[i-1] + pre[i - 1];
         }
-        for(auto x : pre)cout<<x<<" ";
-        cout<<endl;
+        // for(auto x : pre)cout<<x<<" ";
+        // cout<<endl;
         int m = -1, p = -1, g = -1;
-        int cntm = 0;
-        int cntg = 0;
-        int cntp = 0;
+        int cnt= 0 ;
         
         for (int i = n - 1; i >= 0; i--) {
             string s = garbage[i];
             if (s.find('M') != string::npos) {
-                cntm += count(s.begin(), s.end(), 'M');
+                cnt+= count(s.begin(), s.end(), 'M');
                 if (m == -1) m = i;
             }
             if (s.find('P') != string::npos) {
-                cntp += count(s.begin(), s.end(), 'P');
+                cnt += count(s.begin(), s.end(), 'P');
                 if (p == -1) p = i;
             }
             if (s.find('G') != string::npos) {
-                cntg += count(s.begin(), s.end(), 'G');
+                cnt += count(s.begin(), s.end(), 'G');
                 if (g == -1) g = i;
             }
         }
@@ -34,9 +32,7 @@ public:
         if (m != -1) sum += pre[m];
         if (p != -1) sum += pre[p];
         if (g != -1) sum += pre[g];
-        sum += cntm;
-        sum += cntp;
-        sum += cntg;
+        sum+=cnt;
         return sum;
     }
 };
