7ea6412fe2efdc981becc3e43c417735fc0c6103
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-11-20 10:43:38 +0530

    Time: 142 ms (94.14%) | Memory: 83 MB (89.51%) - LeetSync

--- 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.cpp
+++ 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int reductionOperations(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        int ans=0,j=0;
-        for(int i=1;i<nums.size();i++){
-			if(nums[i]!=nums[i-1]){  //if number is not equal to previous then we need one more step so we increase j
-                j++;
-            }
-            ans+=j; //count or every number present in array
+        int ans=0;
+        int n = nums.size();
+        for(int i = n-1 ; i >= 1 ; i--)
+        {
+            if(nums[i]==nums[i-1])continue;
+            ans+=(n-i);
         }
         return  ans;
     }
