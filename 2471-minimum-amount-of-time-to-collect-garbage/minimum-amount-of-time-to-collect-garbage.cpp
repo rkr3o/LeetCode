b6b35a125d85ea939fa82bfc9e330dc868acea91
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-11-20 19:07:01 +0530

    Time: 181 ms (27.28%) | Memory: 102.4 MB (58.71%) - LeetSync

--- 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.cpp
+++ 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.cpp
@@ -1,38 +1,22 @@
-
-class Solution
-{
-    public:
-    int solve(vector<string> &garbage, vector<int> &time)
-    {
-        int cnt = 0;
-        int n = time.size();
-        for (int i = 1; i < n; ++i)
-        {
-            time[i] += time[i - 1];
+class Solution {
+public:
+    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+        int g=-1,p=-1,m=-1,cnt=0;
+        int n = travel.size();
+        for(int i=1;i<n;i++){
+            travel[i]+=travel[i-1];
         }
-        //print(time);
-        int m = -1, p = -1, g = -1;
-        for (int i = 0; i < garbage.size(); ++i)
-        {
-            string t = garbage[i];
-            cnt += t.size();
-            for (auto x: t)
-            {
-                if (x == 'P') p = i;
-                if (x == 'G') g = i;
-                if (x == 'M') m = i;
+        for(int i=garbage.size()-1;i>=0;i--){
+            cnt += garbage[i].size();
+            for(int j = 0;j<garbage[i].size();j++){
+                if(garbage[i][j]=='M' && m==-1)m=i;
+                if(garbage[i][j]=='P' && p==-1)p=i;
+                if(garbage[i][j]=='G' && g==-1)g=i;
             }
         }
-        // print(time);
-        // cout << cnt << endl;
-        // cout << p << " " << m << " " << g << endl;
-        if (p > 0) cnt += time[p - 1];
-        if (g > 0) cnt += time[g - 1];
-        if (m > 0) cnt += time[m - 1];
+        if(m>0)cnt+=travel[m-1];
+        if(p>0)cnt+=travel[p-1];
+        if(g>0)cnt+=travel[g-1];
         return cnt;
-    }
-    int garbageCollection(vector<string> &garbage, vector<int> &time)
-    {
-        return solve(garbage, time);
     }
 };
