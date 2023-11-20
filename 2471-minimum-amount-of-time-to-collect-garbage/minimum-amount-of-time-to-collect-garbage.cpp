527771c4817c9b031a159a2a710610e917899ba6
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-11-20 12:21:15 +0530

    Time: 144 ms (97.46%) | Memory: 102.5 MB (58.71%) - LeetSync

--- 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.cpp
+++ 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.cpp
@@ -1,42 +1,38 @@
-class Solution {
-public:
-    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
-        int n = garbage.size();
-        vector<int> pre(travel.size()+1);
-        pre[0] = 0;
-        for (int i = 1; i <= travel.size(); i++) {
-            pre[i] = travel[i-1] + pre[i - 1];
+
+class Solution
+{
+    public:
+    int solve(vector<string> &garbage, vector<int> &time)
+    {
+        int cnt = 0;
+        int n = time.size();
+        for (int i = 1; i < n; ++i)
+        {
+            time[i] += time[i - 1];
         }
-        for(auto x : pre)cout<<x<<" ";
-        cout<<endl;
+        //print(time);
         int m = -1, p = -1, g = -1;
-        int cntm = 0;
-        int cntg = 0;
-        int cntp = 0;
-        
-        for (int i = n - 1; i >= 0; i--) {
-            string s = garbage[i];
-            if (s.find('M') != string::npos) {
-                cntm += count(s.begin(), s.end(), 'M');
-                if (m == -1) m = i;
-            }
-            if (s.find('P') != string::npos) {
-                cntp += count(s.begin(), s.end(), 'P');
-                if (p == -1) p = i;
-            }
-            if (s.find('G') != string::npos) {
-                cntg += count(s.begin(), s.end(), 'G');
-                if (g == -1) g = i;
+        for (int i = 0; i < garbage.size(); ++i)
+        {
+            string t = garbage[i];
+            cnt += t.size();
+            for (auto x: t)
+            {
+                if (x == 'P') p = i;
+                if (x == 'G') g = i;
+                if (x == 'M') m = i;
             }
         }
-        //cout<<m<<p<<g<<endl;
-        int sum = 0;
-        if (m != -1) sum += pre[m];
-        if (p != -1) sum += pre[p];
-        if (g != -1) sum += pre[g];
-        sum += cntm;
-        sum += cntp;
-        sum += cntg;
-        return sum;
+        // print(time);
+        // cout << cnt << endl;
+        // cout << p << " " << m << " " << g << endl;
+        if (p > 0) cnt += time[p - 1];
+        if (g > 0) cnt += time[g - 1];
+        if (m > 0) cnt += time[m - 1];
+        return cnt;
+    }
+    int garbageCollection(vector<string> &garbage, vector<int> &time)
+    {
+        return solve(garbage, time);
     }
 };
