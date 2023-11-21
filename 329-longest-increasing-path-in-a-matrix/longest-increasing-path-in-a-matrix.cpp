438f250d35d45e9e0c764d5365f33ee51e92a687
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-11-21 12:41:41 +0530

    Time: 39 ms (51.02%) | Memory: 16.3 MB (73.59%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -1,40 +1,38 @@
 class Solution {
-    int dfs(int row , int col , vector<vector<int>>&matrix,vector<vector<int>>&dp)
-    {
-        int cnt = 0;
+public:
+    int solve(int row, int col, vector<vector<int>>& grid, vector<vector<int>>& dp) {
+        if (dp[row][col] != -1) {
+            return dp[row][col];
+        }
+        int n = grid.size();
+        int m = grid[0].size();
+        int drow[] = {-1, 0, +1, 0};
+        int dcol[] = {0, +1,0, -1};
+        int maxLen = 1;
+        for (int i = 0; i < 4; i++) {
+            int nrow = row + drow[i];
+            int ncol = col + dcol[i];
+            if (nrow >= 0 and ncol >= 0 and nrow < n and ncol < m and grid[nrow][ncol] > grid[row][col]) {
+                int len = 1 + solve(nrow, ncol, grid, dp);
+                maxLen = max(maxLen, len);
+            }
+        }
+        dp[row][col] = maxLen;
+        return maxLen;
+    }
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        if (matrix.empty() || matrix[0].empty()) {
+            return 0;
+        }
         int n = matrix.size();
         int m = matrix[0].size();
-        int drow[]={-1,0,+1,0};
-        int dcol[]={0,+1,0,-1};
-        if(dp[row][col]!=-1)return dp[row][col];
-        for(int i = 0 ; i < 4 ; i++)
-        {
-             int nrow = row+drow[i];
-             int ncol = col+dcol[i];
-             if(nrow>=0 and ncol >=0 and nrow<n and ncol<m and matrix[nrow][ncol]>matrix[row][col])
-             {
-                 cnt = max(cnt,dfs(nrow,ncol,matrix,dp));
-             }
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        int maxima = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                maxima = max(maxima, solve(i, j, matrix, dp));
+            }
         }
-        return dp[row][col] = cnt+1;
-    }
-public:
-    int longestIncreasingPath(vector<vector<int>>& matrix) 
-    {
-         int n = matrix.size();
-         int m = matrix[0].size();
-         int maxi = 0 ;
-         //vector<vector<int>>vis(n,vector<)
-         if(matrix.size()==0)return 0;
-         vector<vector<int>>dp(n,vector<int>(m,-1));
-         for(int i = 0 ; i < n ; i++)
-         {
-             for(int j = 0 ; j < m  ; j++)
-             {
-                   int path = dfs(i,j,matrix,dp);
-                   maxi = max(maxi,path);
-             }
-         }
-         return maxi;
+        return maxima;
     }
 };
