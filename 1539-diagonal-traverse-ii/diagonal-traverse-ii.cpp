a557e73d4df53f74ad33c9d232a31fa5118528e1
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-11-23 23:18:59 +0530

    Time: 315 ms (8.86%) | Memory: 118.3 MB (6.96%) - LeetSync

--- 1539-diagonal-traverse-ii/diagonal-traverse-ii.cpp
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
-          vector<int> answer;
-        unordered_map<int, vector<int>> m;
-        int maxKey = 0; // maximum key inserted into the map i.e. max value of i+j indices.
+    vector<int> findDiagonalOrder(vector<vector<int>>& nums) 
+    {
+        if(nums.empty()) return {};
+        vector<int> ans;
+        map<int, vector<int>> mp;
         
-        for (int i=0; i<nums.size(); i++) {
-            for (int j=0; j<nums[i].size(); j++) {
-                m[i+j].push_back(nums[i][j]); // insert nums[i][j] in bucket (i+j).
-                maxKey = max(maxKey, i+j); // 
-            }
-        }
-        for (int i=0; i<= maxKey; i++) { // Each diagonal starting with sum 0 to sum maxKey.
-            for (auto x = m[i].rbegin(); x != m[i].rend(); x++) { // print in reverse order.
-                answer.push_back(*x); 
+        for(int i = 0; i < nums.size(); i++)
+        {
+            for(int j = 0; j < nums[i].size(); j++)
+            {
+                mp[i + j].push_back(nums[i][j]);
             }
         }
         
-        return answer;
+        for(auto x : mp)
+        {
+            vector<int> p = x.second;
+            reverse(p.begin(), p.end());
+            ans.insert(ans.end(), p.begin(), p.end()); 
+        }
+        return ans;
     }
 };
