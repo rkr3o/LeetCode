dc650d93bef70754881fdfb1f0574b574ce89e51
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-11-26 13:03:39 +0530

    Time: 376 ms (5.68%) | Memory: 288.8 MB (5.11%) - LeetSync

--- 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.cpp
+++ 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.cpp
@@ -1,52 +1,62 @@
 class Solution {
 public:
-    int largestSubmatrix(vector<vector<int>>& v) {
-        int n=v.size();
-        int m=v[0].size();
-        int res;
-        vector<int> vv(m,0);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(v[i][j]==1){
-                   vv[j]+=1;
-                }
-                else{
-                    vv[j]=0;
-                }
-            }
-            auto b=vv;//this is the step actually which might be missed
-            sort(b.begin(),b.end());
-            res=max(res,helper(b));
-        }
-        return res;
-    }
-    int helper(vector<int>& heights){
-        int n=heights.size();
+    void getLeftSmall(vector<int>& nums, vector<int>& left) {
+        int n = nums.size();
         stack<int> st;
-        vector<int> prevsmaller(n,-1),nextsmaller(n,n);
-        for(int i=0;i<n;i++){
-            while(st.empty()==false and heights[st.top()]>heights[i]){
-                nextsmaller[st.top()]=i;
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && nums[st.top()] >= nums[i]) {
                 st.pop();
             }
+            if (st.empty()) left[i] = -1;
+            else left[i] = st.top();
             st.push(i);
         }
-        for(int i=n-1;i>=0;i--){
-            while(st.empty()==false and heights[st.top()]>heights[i]){
-                prevsmaller[st.top()]=i;
+    }
+    void getRightSmall(vector<int>& nums, vector<int>& right) {
+        int n = nums.size();
+        stack<int> st;
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && nums[st.top()] > nums[i]) {
                 st.pop();
             }
+            if (st.empty()) right[i] = n;
+            else right[i] = st.top();
             st.push(i);
         }
-      
-        int area=0;
-        for(int i=0;i<=n-1;i++){
-            int l=prevsmaller[i];
-            int r=nextsmaller[i];
-            //cout<<l<<" "<<r<<" "<<heights[i]<<endl;
-            //in between l and r we can make a histogram of atleast height: heights[i]
-            area=max(area,(r-l-1)*(heights[i]));
+    }
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> leftSmall(n, 0);
+        vector<int> rightSmall(n, 0);
+        getLeftSmall(arr, leftSmall);
+        getRightSmall(arr, rightSmall);
+        int area = 0;
+        for (int i = 0; i < n; i++) {
+            int l = leftSmall[i];
+            int r = rightSmall[i];
+            area = max(area, (r - l - 1) * arr[i]);
         }
         return area;
+    }
+    int largestSubmatrix(vector<vector<int>>& mat) {
+        if (mat.empty() || mat[0].empty()) return 0;
+        int n = mat.size();
+        int m = mat[0].size();
+        int maxArea = 0;
+        vector<int> height(m, 0);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 1) { // Compare with integer 1, not character '1'
+                    height[j]++;
+                } else {
+                    height[j] = 0;
+                }
+            }
+            auto b = height;
+            sort(begin(b), end(b));
+            int area = sumSubarrayMins(b);
+            maxArea = max(maxArea, area);
+        }
+        return maxArea;
     }
 };
