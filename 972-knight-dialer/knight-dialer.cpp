c15db67ed5e6ab827bca15cfb1f5d8b5b6db7330
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-11-27 22:30:45 +0530

    Time: 194 ms (29.82%) | Memory: 31.2 MB (48.68%) - LeetSync

--- 972-knight-dialer/knight-dialer.cpp
+++ 972-knight-dialer/knight-dialer.cpp
@@ -1,37 +1,41 @@
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
-    int dp[5][4][5001];
-    int mod=1000000007;
-    int solve(int k,int i,int j)
-    {
-        if(i<0 || j<0 || i>=4 || j>=3 || (i==3 && (j==0 || j==2)))                            //5th OR cond for * and # (:
-            return 0;
-        if(k==1)
-            return 1;
-        
-        if(dp[i][j][k]!=-1)
-            return dp[i][j][k];
-        
-        int a=solve(k-1,i+1,j-2)%mod;
-            a=(a+solve(k-1,i+1,j+2))%mod;
-            a=(a+solve(k-1,i+2,j-1))%mod;
-            a=(a+solve(k-1,i+2,j+1))%mod;
-            a=(a+solve(k-1,i-1,j-2))%mod;
-            a=(a+solve(k-1,i-1,j+2))%mod;
-            a=(a+solve(k-1,i-2,j-1))%mod;
-            a=(a+solve(k-1,i-2,j+1))%mod;
-            
-        return dp[i][j][k]=a;
+    const int mod = 1e9 + 7;
+
+    int solve(int row, int col, int k, vector<vector<vector<int>>>& dp) {
+        if (row == 3 && (col == 0 || col == 2)) return 0;
+        if (k == 0) return 1;
+        if (dp[row][col][k] != -1) return dp[row][col][k];
+
+        int ways = 0;
+        int drow[] = {1, 1, -1, -1, 2, -2, 2, -2};
+        int dcol[] = {2, -2, 2, -2, 1, 1, -1, -1};
+
+        for (int i = 0; i < 8; i++) {
+            int nrow = row + drow[i];
+            int ncol = col + dcol[i];
+            if (nrow >= 0 && ncol >= 0 && nrow < 4 && ncol < 3) {
+                ways = (ways + solve(nrow, ncol, k - 1, dp)) % mod;
+            }
+        }
+
+        return dp[row][col][k] = ways;
     }
-    int knightDialer(int n)
-    {
-       int ans=0;
-        memset(dp,-1,sizeof(dp));
-       for(int i=0;i<4;i++)                                                       // dial Pad
-           for(int j=0;j<3;j++)
-               ans=(ans+solve(n,i,j))%mod;
+
+    int knightDialer(int n) {
+        vector<vector<vector<int>>> dp(4, vector<vector<int>>(3, vector<int>(n + 1, -1)));
+        int ans = 0;
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == 3 && (j == 0 || j == 2)) continue;
+                ans = (ans + solve(i, j, n - 1, dp)) % mod;
+            }
+        }
+
         return ans;
     }
 };
-
-// Feel free to ask any doubt in comment section
