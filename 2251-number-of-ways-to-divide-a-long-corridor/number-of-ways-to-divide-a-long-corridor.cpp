be269c77e8c42a9946c44cafcfa3d7a734403794
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-11-28 13:41:30 +0530

    Time: 147 ms (46.29%) | Memory: 56.1 MB (28.00%) - LeetSync

--- 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.cpp
+++ 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.cpp
@@ -1,30 +1,22 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     const int mod = 1e9 + 7;
-
+    //store the seat index of s , first will show odd pos will be start and even pos will be end
     int numberOfWays(string corridor) {
         vector<int> seatIndices;
         int cnt = 0;
-
         for (int i = 0; i < corridor.size(); i++) {
             if (corridor[i] == 'S') {
                 seatIndices.push_back(i);
                 cnt++;
             }
         }
-
         if (cnt % 2 != 0 || cnt == 0) return 0;
 
         long long res = 1;
-
         for (int i = 2; i < seatIndices.size(); i += 2) {
             res = (res * (seatIndices[i] - seatIndices[i - 1])) % mod;
         }
-
         return static_cast<int>(res);
     }
 };
