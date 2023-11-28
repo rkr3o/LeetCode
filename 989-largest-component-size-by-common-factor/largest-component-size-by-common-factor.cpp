6fc52311b92d2bd2cfa9b1a1f1ab16c297cda8ff
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-11-29 01:48:48 +0530

    Time: 416 ms (33.20%) | Memory: 67.1 MB (65.61%) - LeetSync

--- 989-largest-component-size-by-common-factor/largest-component-size-by-common-factor.cpp
+++ 989-largest-component-size-by-common-factor/largest-component-size-by-common-factor.cpp
@@ -1,9 +1,9 @@
 class DSU {
 public:
-    vector<int> rank, parent, size;
+    vector<int> size, parent;
 
     DSU(int n) {
-        rank.resize(n + 1, 0);
+        size.resize(n + 1, 1);
         parent.resize(n + 1);
 
         for (int i = 0; i <= n; i++) {
@@ -16,46 +16,35 @@ public:
         return parent[v] = find(parent[v]);
     }
 
-    void unionByRank(int u, int v) {
+    void unionBySize(int u, int v) {
         u = find(u);
         v = find(v);
         if (u != v) {
-            if (rank[u] < rank[v]) {
+            if (size[u] < size[v]) {
                 swap(u, v);
             }
             parent[v] = u;
-            if (rank[u] == rank[v]) {
-                rank[u]++;
-            }
+            size[u] += size[v];
         }
     }
 };
 
 class Solution {
-private:
-    void sieve(int maxNum, vector<vector<int>>& factors) {
-        for (int i = 2; i <= maxNum; i++) {
-            if (factors[i].empty()) {  // i is a prime number
-                for (int j = i; j <= maxNum; j += i) {
-                    factors[j].push_back(i);
-                }
-            }
-        }
-    }
 public:
     int largestComponentSize(vector<int>& nums) {
         int maxNum = *max_element(begin(nums), end(nums));
         int n = nums.size();
         DSU ds(maxNum);
 
-        for(int i=0;i<nums.size();i++){
-            for(int j=2;j<=sqrt(nums[i]);j++){
-                if(nums[i]%j==0){
-                    ds.unionByRank(nums[i],j);
-                    ds.unionByRank(nums[i],nums[i]/j);
+        for(int i = 0; i < nums.size(); i++) {
+            for(int j = 2; j <= sqrt(nums[i]); j++) {
+                if(nums[i] % j == 0) {
+                    ds.unionBySize(nums[i], j);
+                    ds.unionBySize(nums[i], nums[i] / j);
                 }
             }
         }
+
         unordered_map<int, int> mp;
         for (auto& x : nums) {
             mp[ds.find(x)]++;
