9402b69259b5282bcd004cda4dcfd12d765fff92
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-12-11 22:10:24 +0530

    Time: 2 ms (77.08%) | Memory: 7 MB (69.88%) - LeetSync

---
+++ 768-partition-labels/partition-labels.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+      vector<int>ans;
+      int n = s.size();
+      vector<int>ind(26,0);
+      for(int i = 0 ; i < n ; i++)
+      {
+          ind[s[i]-'a']=i;
+      } 
+      int maxima = 0 ;
+      cout<<ind[0]<<endl;
+      int prev = 0 ;
+      int sum = 0 ;
+      for(int i = 0 ; i < n ; i++)
+      {
+          maxima = max(maxima,ind[s[i]-'a']);
+          if(i==maxima){
+              if(ans.empty()){
+                  ans.push_back(i+1);
+                  prev=i;
+              }
+              else {
+                  ans.push_back(i-prev);
+                  prev=i;
+              }
+              maxima=0;
+          }
+      }
+       return ans;
+    }
+};
