2eb98856d418c620f7bc1d79a101182cf749b772
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-12-11 21:03:48 +0530

    Time: 26 ms (75.67%) | Memory: 11.1 MB (81.42%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int dp[1000][1000];
+    int solve(int i , int j , string &s , string &t)
+    {
+        if(j<0)return 1;
+        if(i<0)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        int ways = 0 ;
+        if(s[i]==t[j])
+        {
+             dp[i][j] = ways+=solve(i-1,j-1,s,t)+solve(i-1,j,s,t);
+        }
+        else
+        {
+             dp[i][j] = ways=max(ways,solve(i-1,j,s,t));
+        }
+        return dp[i][j] = ways;
+    }
+    int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        memset(dp,-1,sizeof(dp));
+        return solve(n-1,m-1,s,t);
+    }
+};
