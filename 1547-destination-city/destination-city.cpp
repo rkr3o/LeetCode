a620a3e64fa5520041c2b3008131a1bfb48d2c79
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-12-15 11:19:44 +0530

    Time: 11 ms (50.48%) | Memory: 11.8 MB (23.80%) - LeetSync

---
+++ 1547-destination-city/destination-city.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        unordered_map<string,int>mp;
+        for(auto x : paths)
+        {
+            mp[x[0]]+=1;
+            mp[x[1]]+=0;
+        }
+        for(auto &x : mp)
+        {
+            if(x.second==0)
+            return x.first;
+        }
+        return "";
+    }
+};
