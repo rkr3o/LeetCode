0dc5a3bf1434502b594e95fedf523b4ecbe5e903
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2023-12-18 17:54:45 +0530

    Time: 417 ms (73.22%) | Memory: 122.1 MB (56.00%) - LeetSync

--- 1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.cpp
+++ 1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
     vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
-        unordered_map<int, int> result;
-		vector<int> res;
-		priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>  > pq;
-		int i = 0;
-		int n = intervals.size();
-        vector<int> copyQueries = queries;
-		
-        sort(queries.begin(), queries.end());
-        sort(intervals.begin(), intervals.end());
-   				
-        for (int q: queries) {
+        int n = intervals.size();
+        vector<int> ans;
+        unordered_map<int, int> res;
+        vector<int> copyQuery = queries;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        int i = 0;
+        sort(begin(copyQuery), end(copyQuery));
+        sort(begin(intervals), end(intervals));
+
+        for (auto q : copyQuery) {
             while (i < n && intervals[i][0] <= q) {
-                if(intervals[i][1] >= q){
-                    int size = intervals[i][1] - intervals[i][0] + 1;
-                    pq.push({size, intervals[i][1]});
-                }
+                int val = abs(intervals[i][0] - intervals[i][1]) + 1;
+                pq.push({val, intervals[i][1]});
                 i++;
             }
             while (!pq.empty() && pq.top().second < q) {
-                    pq.pop();
+                pq.pop();
             }
             if (!pq.empty()) {
-                result[q] = pq.top().first;
+                res[q] = pq.top().first;
             } else {
-                result[q]= -1;
+                res[q] = -1;
             }
         }
-        for (int i = 0; i < copyQueries.size(); i++) {
-            res.push_back(result[copyQueries[i]]);
+
+        for (int i = 0; i < queries.size(); i++) {
+            ans.push_back(res[queries[i]]);
         }
-        return res;
+        return ans;
     }
 };
