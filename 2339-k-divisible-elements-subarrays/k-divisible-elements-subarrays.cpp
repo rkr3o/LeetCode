14b408e8c344b5f7b0d16f38fe6fffc1de7ba9ff
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2024-01-08 11:50:23 +0530

    Time: 991 ms (66.94%) | Memory: 232.6 MB (61.53%) - LeetSync

--- 2339-k-divisible-elements-subarrays/k-divisible-elements-subarrays.cpp
+++ 2339-k-divisible-elements-subarrays/k-divisible-elements-subarrays.cpp
@@ -6,14 +6,15 @@ public:
         for(int i = 0 ; i < n ; i++)
         {
             vector<int>temp;
-            int cnt = 0 ;
+            int cnt = 0;
             for(int j = i ; j < n ; j++)
             {
-               temp.push_back(nums[j]);
-               if(nums[j]%p==0)cnt++;
-               if(cnt>k)break;
-               st.insert(temp);
+                temp.push_back(nums[j]);
+                if(nums[j]%p==0)cnt++;
+                if(cnt>k)break;
+                  st.insert(temp);
             }
+          
         }
         return st.size();
     }
