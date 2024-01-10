3a730bee56480b00d035db4088fd05fa2bee2d71
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2024-01-10 13:39:52 +0530

    Time: 22 ms (70.63%) | Memory: 15.7 MB (43.65%) - LeetSync

--- 638-shopping-offers/shopping-offers.cpp
+++ 638-shopping-offers/shopping-offers.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
-    map<pair<int, vector<int>>, int> mp;
-    int solve(int ind, vector<int>& price, vector<int> needs, vector<vector<int>>& special) {
-        if (ind == special.size()) {
-            int res = 0;
-            for (int i = 0; i < needs.size(); i++) {
-                res += needs[i] * price[i];
-            }
-            return res;
+int calculateRegularCost(vector<int>& price, vector<int>& needs) {
+        int cost = 0;
+        for (int i = 0; i < price.size(); ++i) {
+            cost += price[i] * needs[i];
         }
-        if (mp.find({ind, needs}) != mp.end()) return mp[{ind, needs}];
-        // Reject offer
-        int reject = solve(ind + 1, price, needs, special);
-        // Apply offer
-        vector<int> originalNeeds = needs;
-        bool canUseOffer = true;
-        for (int i = 0; i < needs.size(); i++) {
-            needs[i] = needs[i] - special[ind][i]; // Decrement the need;
-            if (needs[i] < 0) {
-                return mp[{ind, originalNeeds}] = reject; // Can't use this offer
-            }
-        }
-        int accept = special[ind][needs.size()] + solve(ind, price, needs, special);
-        return mp[{ind, originalNeeds}] = min(accept, reject);
+        return cost;
     }
+int minCost(vector<int>& price, vector<vector<int>>& special, vector<int>& needs,  map<vector<int>, int>& dp) {
+        if (dp.count(needs)) {
+            return dp[needs];
+        }
 
+        int regularCost = calculateRegularCost(price, needs);
+
+        for (auto& offer : special) {
+            vector<int> updatedNeeds(needs.begin(), needs.end());
+            bool validOffer = true;
+            for (int i = 0; i < needs.size(); ++i) {
+                updatedNeeds[i] -= offer[i];
+                if (updatedNeeds[i] < 0) {
+                    validOffer = false;
+                    break;
+                }
+            }
+            if (validOffer) {
+                regularCost = min(regularCost, offer.back() + minCost(price, special, updatedNeeds, dp));
+            }
+        }
+        return dp[needs] = regularCost;
+    }
     int shoppingOffers(vector<int>& price, vector<vector<int>>& special, vector<int>& needs) {
-        return solve(0, price, needs, special);
+        map<vector<int>, int> dp;
+        return minCost(price, special, needs, dp);
     }
 };
