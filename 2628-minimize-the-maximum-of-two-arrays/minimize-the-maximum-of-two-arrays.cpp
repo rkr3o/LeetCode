37c5868f35bf7633777b8f449226c07cdc0c250c
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2024-01-11 11:33:57 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (76.12%) - LeetSync

--- 2628-minimize-the-maximum-of-two-arrays/minimize-the-maximum-of-two-arrays.cpp
+++ 2628-minimize-the-maximum-of-two-arrays/minimize-the-maximum-of-two-arrays.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     long long lcm(int x, int y){
         long long a = x, b = y; 
-        return (a* b) / __gcd(a, b); 
+        return (a*b) / __gcd(a, b); 
     }
     int minimizeSet(int div1, int div2, int cnt1, int cnt2) {
         // a -> No of elem that are not divisible by div1
@@ -15,8 +15,9 @@ public:
         while (low <= high){
             long long mid = low + (high - low) / 2;
             long long a = mid - mid / div1; 
-            long long  b = mid - mid / div2; 
-            long long c = mid - mid / div1 - mid / div2 + mid / lcm(div1, div2); 
+            long long b = mid - mid / div2; 
+            long long c = mid - ((mid / div1) + (mid / div2)) + (mid / lcm(div1, div2)); 
+
             if (a >= cnt1 && b >= cnt2 && a + b - c >= cnt1 + cnt2){
                 ans = mid; 
                 high = mid - 1; 
