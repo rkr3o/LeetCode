05266379927e046fec9fee18be1c9dd972213f32
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2024-01-12 10:05:09 +0530

    Time: 0 ms (100.00%) | Memory: 7 MB (72.68%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
@@ -10,16 +10,15 @@ public:
         if(s.size()%2==1)return false;
         int n = s.size();
         int cnt = 0;
+        int cnt2 = 0 ;
         for(int i =0 ; i < n/2 ; i++)
         {
-            if(isVowel(s[i]))cnt++;
-        }
-        int cnt2 = 0 ;
-        for(int i = n/2; i<n ; i++)
-        {
             if(isVowel(s[i]))
+                cnt++;
+            if(isVowel(s[n-i-1]))
                 cnt2++;
         }
+        
         return cnt==cnt2;
     }
 };
