90570c41b866dbde43b4461ffecebce27f0dea17
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2024-04-27 10:53:28 +0530

    Time: 10 ms (76.48%) | Memory: 19 MB (7.68%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int>mp;
-        for(auto &x:nums)mp[x]++;
-        priority_queue< pair<int,int> , vector<pair<int,int>> ,greater<pair<int,int>>>pq;
         vector<int>ans;
-        
-        for(auto &[x,y]:mp)
-        {
-            pq.push({y,x});
-            if(pq.size()>k)
-            {
-                pq.pop();
-            }
+        unordered_map<int,int>mp;
+        vector<vector<int>>bucket(nums.size()+1);
+        for(auto x : nums)
+            mp[x]++;
+        for(auto &[x,y] : mp){
+            bucket[y].push_back(x);
         }
-        while(!pq.empty()){
-            ans.push_back(pq.top().second);
-            pq.pop();
+        //use bucket sort
+        for(int i = bucket.size()-1; i >=0 ; --i){
+            vector<int>curr_bucket_elem=bucket[i];
+            if(curr_bucket_elem.size() > 0)
+            {
+                int j = 0;
+                while(ans.size()<k and j<curr_bucket_elem.size())
+                {
+                    ans.push_back(curr_bucket_elem[j++]);
+                }
+            }
         }
         return ans;
     }
