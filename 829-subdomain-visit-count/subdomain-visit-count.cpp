ae7c0df35f16d771ff695e661e6f3e428eeec9d7
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2024-07-10 08:50:44 +0530

    Time: 5 ms (93.27%) | Memory: 14.9 MB (91.70%) - LeetSync

---
+++ 829-subdomain-visit-count/subdomain-visit-count.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> subdomainVisits(vector<string>& cpdomains) {
+        map<string, int> domainCount;
+        
+        for (const auto& cpdomain : cpdomains) {
+            int spaceIndex = cpdomain.find(' ');
+            int count = stoi(cpdomain.substr(0, spaceIndex));
+            string domain = cpdomain.substr(spaceIndex + 1);
+            
+            while (true) {
+                domainCount[domain] += count;
+                int dotIndex = domain.find('.');
+                if (dotIndex == string::npos) break;
+                domain = domain.substr(dotIndex + 1);
+            }
+        }
+        
+        vector<string> result;
+        for (const auto& [domain, count] : domainCount) {
+            result.push_back(to_string(count) + " " + domain);
+        }
+        
+        return result;
+    }
+};
