1e7bb8677b94e00c010ee363d9ea40bce36b56c7
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2024-10-29 12:29:59 +0530

    Time: 75 ms (8.68%) | Memory: 8.1 MB (98.91%) - LeetSync

--- 649-dota2-senate/dota2-senate.cpp
+++ 649-dota2-senate/dota2-senate.cpp
@@ -2,40 +2,48 @@ class Solution {
 public:
     //count d and r
     //check whether the opponent is present before or curr index if yes idx--
-    //remove immediate right opponent
-    void solve(int ind , char ch , string &s , vector<int>&vis)
+    // remove immediate right opponent
+    bool solve(int ind , char ch , string &s)
     {
+        bool left = false;
         while(true)
         {
-            if(s[ind]==ch and vis[ind]==false)
+            if(ind==0)left=true;
+            if(s[ind]==ch)
             {
-                vis[ind]=true;
+                s.erase(s.begin()+ind);
                 break;
             }
             ind=(ind+1)%(int)s.size();
         }
+        return left;
     }
     string predictPartyVictory(string senate) {
         int d = count(senate.begin(),senate.end(),'D');
         int r = senate.size()-d;
         int i = 0 ;
-        int n = senate.size();
-        vector<int>vis(n,false);
         while(r>0 and d>0)
         {
-            if(vis[i]==false)
+            if(senate[i]=='R')
             {
-                if(senate[i]=='R')
+                //find the opponent D
+                bool check = solve((i+1)%(int)senate.size(),'D',senate);
+                d--;
+                //if oppenet is present left side of idx 
+                if(check)
                 {
-                    //find the opponent D
-                    solve((i+1)%(int)senate.size(),'D',senate,vis);
-                    d--;
+                    i--;
                 }
-                else
+            }
+            else
+            {
+                //find the opponent D
+                bool check = solve((i+1)%(int)senate.size(),'R',senate);
+                r--;
+                //if oppenet is present left side of idx 
+                if(check)
                 {
-                    //find the opponent D
-                    solve((i+1)%(int)senate.size(),'R',senate,vis);
-                    r--;
+                    i--;
                 }
             }
             i=(i+1)%(int)senate.size();
