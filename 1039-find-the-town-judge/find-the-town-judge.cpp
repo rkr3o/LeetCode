2762d67b08f13c6234ce6f542efad35938855bfb
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2025-01-15 18:15:44 +0530

    Time: 27 ms (37.84%) | Memory: 73.2 MB (30.24%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
-    int findJudge(int n, vector<vector<int>>& adj) 
-    {
-         vector<int>person(n+1,0);
-         
-         for(auto x: adj)
-         {
-             int u = x[0];
-             int v = x[1];
-             person[u]--;
-             person[v]++;
-         }    
-         
-         for(int i = 1 ;i<=n ; i++)
-         {
-             if(person[i]==n-1)
-               return i ;
-         }
-
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int>ans(n+1,0);
+        for(auto x : trust){
+            ans[x[0]]--;
+            ans[x[1]]++;
+        }
+        for(int i = 1 ; i<=n ; i++)
+        {
+            if(ans[i]==n-1)return i ;
+        }
       return -1;
     }
 };
