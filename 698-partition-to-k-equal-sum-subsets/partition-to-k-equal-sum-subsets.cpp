36a4fbdfb2462a7053364314044de989bd3ed86a
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2025-02-24 00:29:46 +0530

    Time: 2986 ms (5.12%) | Memory: 11.9 MB (65.15%) - LeetSync

--- 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
@@ -1,38 +1,30 @@
+
 class Solution {
 public:
-bool solve(vector<int>& nums,vector<bool>& v,int currSum,int index,int target,int k)
-    {
-        //base case
-        if(k==0)
-            return true;
-        if(currSum>target)
-            return false;
-        if(currSum==target)
-            return solve(nums,v,0,0,target,k-1);
+    bool solve(vector<int>& nums, vector<int>& vis, int index, int k, int curr_sum, int target) {
+        if (k == 1) return true;
+        if (curr_sum == target) 
+            return solve(nums, vis, 0, k - 1, 0, target);
 
-        for(int i=index;i<nums.size();i++)
-        {
-            if(v[i]) continue;
-            v[i]=true;
-            if(solve(nums,v,currSum+nums[i],i+1,target,k))
-                return true;
-            v[i]=false;
-            if(currSum==0) break;
+        for (int i = index; i < nums.size(); i++) {
+            if (!vis[i] && curr_sum + nums[i] <= target) {
+                vis[i] = 1;   
+                if (solve(nums, vis, i + 1, k, curr_sum + nums[i], target)) return true;
+                vis[i] = 0;
+            }
         }
-            return false;
+        return false;
     }
+
     bool canPartitionKSubsets(vector<int>& nums, int k) {
-        int total=0;
-        for(int i=0;i<nums.size();i++)
-            total+=nums[i];
-        if(total%k!=0 || k>nums.size())
-            return false;
-        int target=total/k; //target sum 
-        vector<bool> v(nums.size(),false);
-        sort(nums.begin(),nums.end());
-        if(nums[0]>target)
-            return false;
-        return solve(nums,v,0,0,target,k);
+        int total_sum = accumulate(nums.begin(), nums.end(), 0);
+        if (total_sum % k != 0 || k > nums.size()) return false;
+        
+        int target = total_sum / k;
+        sort(nums.begin(), nums.end());
+        if (nums[0] > target) return false;
+
+        vector<int> vis(nums.size(), 0);
+        return solve(nums, vis, 0, k, 0, target);
     }
-    
 };
