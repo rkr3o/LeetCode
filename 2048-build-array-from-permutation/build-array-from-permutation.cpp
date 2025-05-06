3f22e6ed72e06d0e24eb9c125663bc069df5e363
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2025-05-06 16:03:33 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (58.11%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.cpp
+++ 2048-build-array-from-permutation/build-array-from-permutation.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
+
         int n = nums.size();
         vector<int>ans(n,0);
-        unordered_map<int,int>mp;
-        for(int i=0; i < n; i++){
-            mp[i]=nums[i];
-        }
-        for(int i = 0 ; i < n ;i++){
-            if(mp.find(nums[i])!=mp.end()){
-                ans[i] = mp[nums[i]];
-            }
+        for(int i = 0 ; i < n ; i++){
+            ans[i]=nums[nums[i]];
         }
         return ans;
     }
