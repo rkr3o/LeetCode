35f4b085f7a475e9b36d5db30bdb1f6a93545f34
RAVI KUMAR RANA <63628554+rkr3o@users.noreply.github.com>
2025-08-03 11:07:08 +0530

    Time: 4 ms (73.62%) | Memory: 124.4 MB (96.17%) - LeetSync

--- 2229-maximum-fruits-harvested-after-at-most-k-steps/maximum-fruits-harvested-after-at-most-k-steps.cpp
+++ 2229-maximum-fruits-harvested-after-at-most-k-steps/maximum-fruits-harvested-after-at-most-k-steps.cpp
@@ -2,25 +2,24 @@ class Solution {
 public:
     int maxTotalFruits(vector<vector<int>>& fruits, int startPos, int k) {
         int n = fruits.size();
-        int maxFruits = 0;
-        int left = 0, sum = 0;
+        int sum = 0, maxima = 0;
+        int left = 0, right = 0;
 
-        for (int right = 0; right < n; ++right) {
-            sum += fruits[right][1]; 
-            while (left <= right && !canReach(fruits[left][0], fruits[right][0], startPos, k)) {
+        auto checkReach = [&](int left, int right) {
+            int op1 = abs(left - startPos) + abs(right - left);
+            int op2 = abs(right - startPos) + abs(right - left);
+            return min(op1, op2) <= k;
+        };
+
+        while(right < n ){
+            sum += fruits[right][1];
+            while (left <= right && !checkReach(fruits[left][0], fruits[right][0])) {
                 sum -= fruits[left][1];
                 left++;
             }
-
-            maxFruits = max(maxFruits, sum);
+            maxima = max(sum, maxima);
+            right++;
         }
-
-        return maxFruits;
-    }
-
-    bool canReach(int leftPos, int rightPos, int startPos, int k) {
-        int option1 = abs(startPos - leftPos) + (rightPos - leftPos); 
-        int option2 = abs(startPos - rightPos) + (rightPos - leftPos); 
-        return min(option1, option2) <= k;
+        return maxima;
     }
 };
